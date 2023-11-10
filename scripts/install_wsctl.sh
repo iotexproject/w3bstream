f8543ce25c70cba6221ae7e0d7e313cca9aea310
Raullen Chai <raullenchai@gmail.com>
2023-11-10 14:38:33 -0800

    Update install_wsctl.sh

--- scripts/install_wsctl.sh
+++ scripts/install_wsctl.sh
@@ -1,25 +1,56 @@
 #!/bin/bash
 
-# Set the GitHub API URL for the releases of wsctl
-GITHUB_API_URL="https://api.github.com/repos/machinefi/w3bstream-mainnet/releases/latest"
+# GitHub API URL for the latest release of machinefi/sprout
+GITHUB_API_URL="https://api.github.com/repos/machinefi/sprout/releases/latest"
 
-# Use curl to get the download URL for the latest release of wsctl
-DOWNLOAD_URL=$(curl -s $GITHUB_API_URL | grep "browser_download_url" | cut -d '"' -f 4 | head -n 1)
+# Detect the operating system
+OS="$(uname)"
 
-# Check if the download URL is empty
-if [ -z "$DOWNLOAD_URL" ]; then
-    echo "Error: Could not find the download URL for wsctl."
-    exit 1
-fi
+# Function to download wsctl based on OS
+download_wsctl() {
+    local os_name=$1
+    local binary_name="wsctl-${os_name}-amd64"
+    
+    if [ "$os_name" = "windows" ]; then
+        binary_name="${binary_name}.exe"
+    fi
 
-# Download the latest release of wsctl
-curl -L $DOWNLOAD_URL -o wsctl
+    local download_url=$(curl -s $GITHUB_API_URL | grep "browser_download_url.*$binary_name" | cut -d '"' -f 4 | head -n 1)
 
-# Make the wsctl file executable
-chmod +x wsctl
+    if [ -z "$download_url" ]; then
+        echo "Download URL not found for $binary_name"
+        exit 1
+    fi
 
-# Move wsctl to /usr/local/bin so it can be run from anywhere
+    echo "Downloading wsctl for ${os_name}..."
+    curl -L $download_url -o wsctl
+    chmod +x wsctl
+
+    echo "wsctl downloaded and made executable."
+}
+
+case "$OS" in
+    'Linux')
+        download_wsctl "linux"
+        ;;
+    'Darwin')
+        download_wsctl "darwin"
+        ;;
+    'MINGW'*|'MSYS'*|'CYGWIN'*)
+        download_wsctl "windows"
+        ;;
+    *)
+        echo "Unsupported OS: $OS"
+        exit 1
+        ;;
+esac
+
+# Move wsctl to a directory in PATH (optional, modify as needed)
+# Note: This step may not be applicable or may differ for Windows
 sudo mv wsctl /usr/local/bin/
+
+# Any additional setup or configuration steps
+
 
 # Output the version of wsctl
 echo "wsctl installed successfully."
