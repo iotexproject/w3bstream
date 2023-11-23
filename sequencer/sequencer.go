1d236e71695f8fbf93d6698319c1e26a0e2052f0
huangzhiran <zhiran@iotex.me>
2023-11-23 15:05:57 +0800

    refactor(message): rename message state

--- sequencer/sequencer.go
+++ sequencer/sequencer.go
@@ -1,7 +1,6 @@
 package sequencer
 
 import (
-	"github.com/machinefi/sprout/enums"
 	"github.com/machinefi/sprout/proto"
 	"github.com/pkg/errors"
 	"gorm.io/driver/postgres"
@@ -37,7 +36,7 @@ func (s *Sequencer) Save(msg *proto.Message) error {
 
 func (s *Sequencer) Fetch(projectID uint64) (*proto.Message, error) {
 	m := Message{}
-	result := s.db.Where("project_id = ? AND state = ?", projectID, enums.MessageReceived).First(&m)
+	result := s.db.Where("project_id = ? AND state = ?", projectID, proto.MessageState_RECEIVED).First(&m)
 	if result.Error != nil {
 		return nil, errors.Wrapf(result.Error, "query message failed, projectID %d", projectID)
 	}
