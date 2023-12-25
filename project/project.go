2acb8643f066fad9258edce741c57d4a9b0a50d4
huangzhiran <zhiran@iotex.me>
2023-12-25 22:04:55 +0800

    feat(project): add project registrar watcher

--- project/project.go
+++ project/project.go
@@ -1,6 +1,13 @@
 package project
 
-import "github.com/machinefi/sprout/types"
+import (
+	"encoding/json"
+	"io"
+	"net/http"
+
+	"github.com/machinefi/sprout/types"
+	"github.com/pkg/errors"
+)
 
 type (
 	Project struct {
@@ -31,3 +38,36 @@ type (
 		} `json:"solana,omitempty"`
 	}
 )
+
+type ProjectMeta struct {
+	ProjectID uint64
+	Uri       string
+	Hash      [32]byte
+	Paused    bool
+}
+
+func (m *ProjectMeta) GetProject() (*Project, error) {
+	resp, err := http.Get(m.Uri)
+	if err != nil {
+		return nil, errors.Wrapf(err, "fetch project config failed, projectID %d, uri %s", m.ProjectID, m.Uri)
+	}
+	defer resp.Body.Close()
+
+	content, err := io.ReadAll(resp.Body)
+	if err != nil {
+		return nil, errors.Wrapf(err, "read project config failed, projectID %d, uri %s", m.ProjectID, m.Uri)
+	}
+	c := &Config{}
+	if err = json.Unmarshal(content, c); err != nil {
+		return nil, errors.Wrapf(err, "parse project config failed, projectID %d, uri %s", m.ProjectID, m.Uri)
+	}
+	// simple validation
+	if len(c.Code) == 0 || c.VMType == "" {
+		return nil, errors.Errorf("invalid project config, projectID %d, uri %s", m.ProjectID, m.Uri)
+	}
+	// TODO validate hash
+	return &Project{
+		ID:     m.ProjectID,
+		Config: *c,
+	}, nil
+}
