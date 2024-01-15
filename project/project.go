aec53e96d83dc496263dd1c0c767817b622ce212
sincos <birdyfj@gmail.com>
2024-01-15 23:04:33 +0800

    feat: support ipfs url with endpoint

--- project/project.go
+++ project/project.go
@@ -7,6 +7,7 @@ import (
 	"io"
 	"net/http"
 	"net/url"
+	"strings"
 
 	"github.com/pkg/errors"
 
@@ -67,20 +68,27 @@ func (m *ProjectMeta) GetConfigs(ipfsEndpoint string) ([]*Config, error) {
 		content []byte
 		err     error
 	)
-	if u, _err := url.Parse(m.Uri); _err == nil && (u.Scheme == "http" || u.Scheme == "https") {
-		// fetch content from ipfs gateway
-		resp, _err := http.Get(m.Uri)
-		if _err != nil {
-			return nil, errors.Wrapf(err, "fetch project config failed, projectID %d, uri %s", m.ProjectID, m.Uri)
-		}
-		defer resp.Body.Close()
-
-		// TODO network error should try again
-		content, err = io.ReadAll(resp.Body)
-	} else {
-		// fetch content by ipfs cid
+	u, _err := url.Parse(m.Uri)
+	if _err != nil {
+		// fetch content by ipfs cid with default endpoint
 		sh := ipfs.NewIPFS(ipfsEndpoint)
 		content, err = sh.Cat(m.Uri)
+	} else {
+		switch u.Scheme {
+		case "http", "https":
+			resp, _err := http.Get(m.Uri)
+			if _err != nil {
+				return nil, errors.Wrapf(err, "fetch project config failed, projectID %d, uri %s", m.ProjectID, m.Uri)
+			}
+			defer resp.Body.Close()
+			// TODO network error should try again
+			content, err = io.ReadAll(resp.Body)
+		case "ipfs":
+			// ipfs url: ipfs://${endpoint}/${cid}
+			sh := ipfs.NewIPFS(u.Host)
+			cid := strings.Split(strings.Trim(u.Path, "/"), "/")
+			content, err = sh.Cat(cid[0])
+		}
 	}
 
 	if err != nil {
