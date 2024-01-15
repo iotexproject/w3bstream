52b80c084b58c18cc07697cae7d7e7adcba30403
saito <birdyfj@gmail.com>
2024-01-15 23:03:13 +0800

    fix: project loading (#211)

--- project/manager.go
+++ project/manager.go
@@ -2,7 +2,6 @@ package project
 
 import (
 	"bytes"
-	"encoding/hex"
 	"encoding/json"
 	"fmt"
 	"log/slog"
@@ -44,6 +43,19 @@ func (m *Manager) Get(projectID uint64, version string) (*Config, error) {
 	return nil, errors.Errorf("project config not exist, projectID %d, version %s", projectID, version)
 }
 
+func (m *Manager) Set(projectID uint64, version string, c *Config) {
+	m.mux.Lock()
+	defer m.mux.Unlock()
+
+	key := getKey(projectID, version)
+	old, ok := m.pool[key]
+	m.pool[key] = c
+	if ok {
+		slog.Warn("project was overwritten", "project_id", projectID, "version", version, "old", old, "new", c)
+	}
+	m.projectIDs[projectID] = true
+}
+
 // TODO will delete when node konw how to fetch message
 func (m *Manager) GetAllProjectID() []uint64 {
 	m.mux.Lock()
@@ -81,23 +93,20 @@ func (m *Manager) watchProjectRegistrar(logs <-chan *types.Log, subs event.Subsc
 				continue
 			}
 
-			m.mux.Lock()
-
 			for _, c := range cs {
-				m.pool[getKey(pm.ProjectID, c.Version)] = c
+				slog.Info("monitor project", "project_id", pm.ProjectID, "version", c.Version, "vm_type", c.VMType, "code_size", len(c.Code))
+				m.Set(pm.ProjectID, c.Version, c)
 			}
-			m.projectIDs[pm.ProjectID] = true
-
-			m.mux.Unlock()
 		}
 	}
 }
 
-func (m *Manager) fillProjectPoolFromLocal(projectFileDirectory string) error {
+func (m *Manager) fillProjectPoolFromLocal(projectFileDirectory string) {
 	files, err := os.ReadDir(projectFileDirectory)
 	if err != nil {
 		if !os.IsNotExist(err) {
-			return errors.Wrap(err, "read project file directory failed")
+			slog.Warn("failed to read project directory", "path", projectFileDirectory, "msg", err)
+			return
 		}
 	}
 	for _, f := range files {
@@ -106,39 +115,43 @@ func (m *Manager) fillProjectPoolFromLocal(projectFileDirectory string) error {
 		}
 		data, err := os.ReadFile(path.Join(projectFileDirectory, f.Name()))
 		if err != nil {
-			return errors.Wrapf(err, "read project file %s failed", f.Name())
+			slog.Error("failed to read project config", "filename", f.Name(), "msg", err)
+			continue
 		}
 
 		projectID, err := strconv.ParseUint(f.Name(), 10, 64)
 		if err != nil {
-			return errors.Wrapf(err, "parse file name %s to projectID failed", f.Name())
+			slog.Error("failed to parse filename", "filename", f.Name())
+			continue
 		}
 
 		cs := []*Config{}
 		if err := json.Unmarshal(data, &cs); err != nil {
-			return errors.Wrapf(err, "unmarshal config file %s failed", f.Name())
+			slog.Error("failed to parse project config", "filename", f.Name())
+			continue
 		}
 
 		for _, c := range cs {
-			m.pool[getKey(projectID, c.Version)] = c
+			slog.Info("local project loaded", "project_id", projectID, "version", c.Version, "vm_type", c.VMType, "code_size", len(c.Code))
+			m.Set(projectID, c.Version, c)
 		}
-		m.projectIDs[projectID] = true
 	}
-	return nil
 }
 
-func (m *Manager) fillProjectPoolFromChain() error {
+func (m *Manager) fillProjectPoolFromChain() {
 	emptyHash := [32]byte{}
 	for i := uint64(1); ; i++ {
 		mp, err := m.instance.Projects(nil, i)
 		if err != nil {
-			return errors.Wrapf(err, "get project meta from chain failed, projectID %d", i)
+			slog.Error("failed to get project meta from chain", "project_id", i, "msg", err)
+			continue
 		}
 		// query empty, means reached the maximum projectID value
 		if mp.Uri == "" || bytes.Equal(mp.Hash[:], emptyHash[:]) {
-			return nil
+			slog.Info("project from contract read completed", "max", i-1)
+			return
 		}
-		slog.Debug("queried project", "project_id", i, "uri", mp.Uri, "hash", hex.EncodeToString(mp.Hash[:]))
+
 		pm := &ProjectMeta{
 			ProjectID: i,
 			Uri:       mp.Uri,
@@ -152,10 +165,15 @@ func (m *Manager) fillProjectPoolFromChain() error {
 		}
 
 		for _, c := range cs {
-			m.pool[getKey(pm.ProjectID, c.Version)] = c
+			slog.Info("contract project loaded", "project_id", pm.ProjectID, "version", c.Version, "vm_type", c.VMType, "code_size", len(c.Code))
+			m.Set(i, c.Version, c)
 		}
-		m.projectIDs[pm.ProjectID] = true
 	}
+}
+
+func (m *Manager) loadProjects(projectFileLocalDir string) {
+	m.fillProjectPoolFromLocal(projectFileLocalDir)
+	m.fillProjectPoolFromChain()
 }
 
 func NewManager(chainEndpoint, contractAddress, projectFileDirectory, ipfsEndpoint string) (*Manager, error) {
@@ -175,15 +193,9 @@ func NewManager(chainEndpoint, contractAddress, projectFileDirectory, ipfsEndpoi
 		return nil, errors.Wrapf(err, "new contract instance failed, endpoint %s, contractAddress %s", chainEndpoint, contractAddress)
 	}
 
-	if err = m.fillProjectPoolFromLocal(projectFileDirectory); err != nil {
-		return nil, errors.Wrap(err, "read project file from local failed")
-	}
-	if err = m.fillProjectPoolFromChain(); err != nil {
-		return nil, errors.Wrap(err, "read project file from chain failed")
-	}
+	go m.loadProjects(projectFileDirectory)
 
 	topic := "ProjectUpserted(uint64,string,bytes32)"
-
 	monitor, err := NewDefaultMonitor(
 		chainEndpoint,
 		[]string{contractAddress},
