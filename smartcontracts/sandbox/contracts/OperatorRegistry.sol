ed4102f85774f538e41f4e36f1e9843c72ad03b9
Nicky Ru <64008830+nicky-ru@users.noreply.github.com>
2024-02-13 19:51:41 +0300

    update operator registry

--- smartcontracts/sandbox/contracts/OperatorRegistry.sol
+++ smartcontracts/sandbox/contracts/OperatorRegistry.sol
@@ -2,69 +2,43 @@
 pragma solidity ^0.8.19;
 
 contract OperatorRegistry {
-    uint256 private _nextOperatorId;
-
-    mapping(uint256 => Operator) public operators;
+    mapping(address => Operator) public operators;
 
     struct Operator {
-        // an uri can be added with operator metadata
-        // name can be in metadata
-        string name;
-        address profile;
         address node;
         address rewards;
     }
 
-    event OperatorRegistered(
-        uint256 indexed operatorId,
-        address indexed profile,
-        address indexed node,
-        address rewards,
-        string name
-    );
-    event OperatorNodeUpdated(uint256 indexed operatorId, address indexed newNode);
-    event OperatorRewardsUpdated(uint256 indexed operatorId, address indexed rewards);
+    event OperatorRegistered(address indexed profile, address indexed node, address rewards);
+    event OperatorNodeUpdated(address indexed profile, address indexed newNode);
+    event OperatorRewardsUpdated(address indexed profile, address indexed rewards);
 
-    modifier onlyExistingOperator(uint256 operatorId) {
-        require(operators[operatorId].profile != address(0), "OperatorRegistry: unexistent operator");
+    modifier onlyExistingOperator() {
+        require(operators[msg.sender].node != address(0), "OperatorRegistry: unexistent operator");
         _;
     }
 
-    modifier onlyOperatorOwner(uint256 operatorId) {
-        require(operators[operatorId].profile == msg.sender, "OperatorRegistry: Not operator owner");
+    modifier onlyNewOperator() {
+        require(operators[msg.sender].node == address(0), "OperatorRegistry: operator already registered");
         _;
     }
 
-    function registerOperator(Operator memory operator) public {
-        require(
-            keccak256(abi.encodePacked(operator.name)) != keccak256(abi.encodePacked("")),
-            "OperatorRegistry: name can't be empty"
-        );
-
-        uint256 operatorId = _nextOperatorId++;
-
-        // Should we extract profileAddress from msg.sender?
-        operators[operatorId] = operator;
-
-        emit OperatorRegistered(operatorId, operator.profile, operator.node, operator.rewards, operator.name);
+    function registerOperator(Operator memory _operator) public onlyNewOperator {
+        address profile = msg.sender;
+        operators[profile] = _operator;
+        emit OperatorRegistered(profile, _operator.node, _operator.rewards);
     }
 
-    function updateNode(
-        uint256 operatorId,
-        address newNode
-    ) public onlyExistingOperator(operatorId) onlyOperatorOwner(operatorId) {
-        operators[operatorId].node = newNode;
-
-        emit OperatorNodeUpdated(operatorId, newNode);
+    function updateNode(address _newNode) public onlyExistingOperator {
+        address profile = msg.sender;
+        operators[profile].node = _newNode;
+        emit OperatorNodeUpdated(profile, _newNode);
     }
 
-    function updateRewards(
-        uint256 operatorId,
-        address newRewards
-    ) public onlyExistingOperator(operatorId) onlyOperatorOwner(operatorId) {
-        operators[operatorId].rewards = newRewards;
-
-        emit OperatorRewardsUpdated(operatorId, newRewards);
+    function updateRewards(address _newRewards) public onlyExistingOperator {
+        address profile = msg.sender;
+        operators[profile].rewards = _newRewards;
+        emit OperatorRewardsUpdated(profile, _newRewards);
     }
 
     // function stake() public {}
