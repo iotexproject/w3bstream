6abdf036d29bfd9852d4a58f15e7b8b956d1c913
Nicky Ru <64008830+nicky-ru@users.noreply.github.com>
2024-02-13 23:52:00 +0300

    update fleet manager

--- smartcontracts/sandbox/contracts/FleetManager.sol
+++ smartcontracts/sandbox/contracts/FleetManager.sol
@@ -1,13 +1,91 @@
 // SPDX-License-Identifier: MIT
 pragma solidity ^0.8.19;
 
-import {Initializable} from "@openzeppelin/contracts-upgradeable/proxy/utils/Initializable.sol";
 import {IFleetManager} from "./interfaces/IFleetManager.sol";
+import {IOperatorRegistry} from "./interfaces/IOperatorRegistry.sol";
+import {IProjectRegistry} from "./interfaces/IProjectRegistry.sol";
 
-contract FleetManager is IFleetManager, Initializable {
-    function initialize() public initializer {}
+contract FleetManager is IFleetManager {
+    address public projectRegistry;
+    address public operatorRegistry;
 
-    function isAllowed(address _operator, uint256 _projectId) external view override returns (bool) {
-        return true;
+    mapping(uint256 => address[]) public operators;
+
+    event OperatorAdded(uint256 indexed projectId, address indexed operator);
+    event OperatorRemoved(uint256 indexed projectId, address indexed operator);
+
+    constructor(address _projectRegistry, address _operatorRegistry) {
+        projectRegistry = _projectRegistry;
+        operatorRegistry = _operatorRegistry;
+    }
+
+    modifier onlyProjectOwner(uint256 _projectId) {
+        require(
+            IProjectRegistry(projectRegistry).isProjectOwner(msg.sender, _projectId),
+            "FleetManager: not project owner"
+        );
+        _;
+    }
+
+    modifier onlyValidOperator(address _operator) {
+        address node = IOperatorRegistry(operatorRegistry).getOperator(_operator).node;
+        require(node != address(0), "FleetManager: operator not registered");
+
+        // NEED TO CHECK IF THE OPERATOR HAS ENOUGH STAKE
+        _;
+    }
+
+    modifier notAlreadyAllowed(uint256 _projectId, address _operator) {
+        address[] memory projectOperators = operators[_projectId];
+
+        for (uint256 i = 0; i < projectOperators.length; i++) {
+            require(projectOperators[i] != _operator, "FleetManager: operator already allowed");
+        }
+        _;
+    }
+
+    function allow(
+        uint256 _projectId,
+        address _operator
+    ) external onlyProjectOwner(_projectId) onlyValidOperator(_operator) notAlreadyAllowed(_projectId, _operator) {
+        operators[_projectId].push(_operator);
+        emit OperatorAdded(_projectId, _operator);
+    }
+
+    function disallow(uint256 _projectId, address _operator) external onlyProjectOwner(_projectId) {
+        address[] storage projectOperators = operators[_projectId];
+
+        bool found;
+
+        for (uint256 i = 0; i < projectOperators.length; i++) {
+            if (projectOperators[i] == _operator) {
+                projectOperators[i] = projectOperators[projectOperators.length - 1];
+                projectOperators.pop();
+                found = true;
+                break;
+            }
+        }
+
+        require(found, "FleetManager: operator not found");
+
+        emit OperatorRemoved(_projectId, _operator);
+    }
+
+    function isAllowed(address _node, uint256 _projectId) external view returns (bool) {
+        require(_node != address(0), "FleetManager: invalid node");
+
+        address[] memory projectOperators = operators[_projectId];
+
+        for (uint256 i = 0; i < projectOperators.length; i++) {
+            if (_getOperatorNode(projectOperators[i]) == _node) {
+                return true;
+            }
+        }
+
+        return false;
+    }
+
+    function _getOperatorNode(address _operator) internal view returns (address) {
+        return IOperatorRegistry(operatorRegistry).getOperator(_operator).node;
     }
 }
---
+++ smartcontracts/sandbox/test/fleet.test.ts
@@ -0,0 +1,167 @@
+import { expect } from 'chai';
+import { loadFixture } from '@nomicfoundation/hardhat-toolbox/network-helpers';
+import { ethers } from 'hardhat';
+import { Signer } from 'ethers';
+import { SignerWithAddress } from '@nomicfoundation/hardhat-ethers/signers';
+
+describe('FleetManager', function () {
+  it('should be initialized with project and operator registry', async function () {
+    const fleet = await loadFixture(deployFleetManager);
+
+    expect(await fleet.projectRegistry()).to.not.eq(ethers.ZeroAddress);
+    expect(await fleet.operatorRegistry()).to.not.eq(ethers.ZeroAddress);
+  });
+
+  const PROJECT_1 = {
+    uri: 'project1',
+    hash: '0x91f11349770aadcc135213916bf429e39f7419b25d5fe6a2623115b35b381389',
+  };
+  const PROJECT_ID_1 = 1;
+
+  describe('isAllowed', function () {
+    it('should return true if sender is allowed', async function () {
+      const fleet = await loadFixture(deployFleetManager);
+      const [projectOwner, profile, node, rewards] = await ethers.getSigners();
+
+      await registerOperator(await fleet.operatorRegistry(), profile, node, rewards);
+      await registerProject(await fleet.projectRegistry(), projectOwner, PROJECT_1.uri, PROJECT_1.hash);
+
+      await fleet.connect(projectOwner).allow(PROJECT_ID_1, profile.address);
+
+      expect(await fleet.isAllowed(node.address, PROJECT_ID_1)).to.be.true;
+    });
+    it('returns false if unexisting project', async function () {
+      const fleet = await loadFixture(deployFleetManager);
+      const [, node] = await ethers.getSigners();
+
+      expect(await fleet.isAllowed(node.address, 1)).to.be.eq(false);
+    });
+    it('reverts if node address is zero', async function () {
+      const fleet = await loadFixture(deployFleetManager);
+
+      await expect(fleet.isAllowed(ethers.ZeroAddress, 1)).to.be.revertedWith('FleetManager: invalid node');
+    });
+  });
+  describe('allow', function () {
+    it('works if called by project owner', async function () {
+      const fleet = await loadFixture(deployFleetManager);
+      const [projectOwner, profile, node, rewards] = await ethers.getSigners();
+
+      await registerOperator(await fleet.operatorRegistry(), profile, node, rewards);
+      await registerProject(await fleet.projectRegistry(), projectOwner, PROJECT_1.uri, PROJECT_1.hash);
+
+      await expect(fleet.connect(projectOwner).allow(PROJECT_ID_1, profile.address))
+        .to.emit(fleet, 'OperatorAdded')
+        .withArgs(PROJECT_ID_1, profile.address);
+
+      expect(await fleet.isAllowed(node.address, PROJECT_ID_1)).to.be.true;
+    });
+    it('reverts if called by non-owner', async function () {
+      const fleet = await loadFixture(deployFleetManager);
+      const [projectOwner, profile] = await ethers.getSigners();
+
+      await registerProject(await fleet.projectRegistry(), projectOwner, PROJECT_1.uri, PROJECT_1.hash);
+
+      await expect(fleet.connect(profile).allow(PROJECT_ID_1, profile.address)).to.be.revertedWith(
+        'FleetManager: not project owner',
+      );
+    });
+    it('reverts if operator is not registered', async function () {
+      const fleet = await loadFixture(deployFleetManager);
+      const [projectOwner, profile] = await ethers.getSigners();
+
+      await registerProject(await fleet.projectRegistry(), projectOwner, PROJECT_1.uri, PROJECT_1.hash);
+
+      await expect(fleet.connect(projectOwner).allow(PROJECT_ID_1, profile.address)).to.be.revertedWith(
+        'FleetManager: operator not registered',
+      );
+    });
+    it('reverts if operator is already allowed', async function () {
+        const fleet = await loadFixture(deployFleetManager);
+        const [projectOwner, profile, node, rewards] = await ethers.getSigners();
+    
+        await registerOperator(await fleet.operatorRegistry(), profile, node, rewards);
+        await registerProject(await fleet.projectRegistry(), projectOwner, PROJECT_1.uri, PROJECT_1.hash);
+    
+        await fleet.connect(projectOwner).allow(PROJECT_ID_1, profile.address);
+        await expect(fleet.connect(projectOwner).allow(PROJECT_ID_1, profile.address)).to.be.revertedWith(
+            'FleetManager: operator already allowed',
+        );
+    });
+  });
+  describe('disallow', function () {
+    it('works if called by project owner', async function () {
+      const fleet = await loadFixture(deployFleetManager);
+      const [projectOwner, profile, node, rewards] = await ethers.getSigners();
+
+      await registerOperator(await fleet.operatorRegistry(), profile, node, rewards);
+      await registerProject(await fleet.projectRegistry(), projectOwner, PROJECT_1.uri, PROJECT_1.hash);
+
+      await fleet.connect(projectOwner).allow(PROJECT_ID_1, profile.address);
+      await expect(fleet.connect(projectOwner).disallow(PROJECT_ID_1, profile.address))
+        .to.emit(fleet, 'OperatorRemoved')
+        .withArgs(PROJECT_ID_1, profile.address);
+
+      expect(await fleet.isAllowed(node.address, PROJECT_ID_1)).to.be.false;
+    });
+    it('reverts if called by non-owner', async function () {
+      const fleet = await loadFixture(deployFleetManager);
+      const [projectOwner, profile] = await ethers.getSigners();
+
+      await registerProject(await fleet.projectRegistry(), projectOwner, PROJECT_1.uri, PROJECT_1.hash);
+
+      await expect(fleet.connect(profile).disallow(PROJECT_ID_1, profile.address)).to.be.revertedWith(
+        'FleetManager: not project owner',
+      );
+    });
+    it('reverts if operator is not allowed', async function () {
+      const fleet = await loadFixture(deployFleetManager);
+      const [projectOwner, profile] = await ethers.getSigners();
+
+      await registerProject(await fleet.projectRegistry(), projectOwner, PROJECT_1.uri, PROJECT_1.hash);
+
+      await expect(fleet.connect(projectOwner).disallow(PROJECT_ID_1, profile.address)).to.be.revertedWith(
+        'FleetManager: operator not found',
+      );
+    });
+  });
+});
+
+async function deployFleetManager() {
+  const projectRegistry = await deployProjectRegistry();
+  const projectRegistryAddress = await projectRegistry.getAddress();
+
+  const operatorRegistry = await deployOperatorRegistry();
+  const operatorRegistryAddress = await operatorRegistry.getAddress();
+
+  const factory = await ethers.getContractFactory('FleetManager');
+  return factory.deploy(projectRegistryAddress, operatorRegistryAddress);
+}
+
+async function deployProjectRegistry() {
+  const factory = await ethers.getContractFactory('ProjectRegistry');
+  return factory.deploy();
+}
+
+async function deployOperatorRegistry() {
+  const factory = await ethers.getContractFactory('OperatorRegistry');
+  return factory.deploy();
+}
+
+async function registerOperator(
+  operatorRegistry: string,
+  profile: Signer,
+  node: SignerWithAddress,
+  rewards: SignerWithAddress,
+) {
+  const registry = await ethers.getContractAt('OperatorRegistry', operatorRegistry);
+  await registry.connect(profile).registerOperator({
+    node: node.address,
+    rewards: rewards.address,
+  });
+}
+
+async function registerProject(projectRegistry: string, projectOwner: Signer, uri: string, hash: string) {
+  const registry = await ethers.getContractAt('ProjectRegistry', projectRegistry);
+  await registry.connect(projectOwner).createProject(uri, hash);
+}
