9ddd7b4994be3cfe2bcbafb375aba7fd2c31a1cb
Nicky Ru <64008830+nicky-ru@users.noreply.github.com>
2024-02-19 02:35:42 +0300

    update wsreceiver

--- smartcontracts/sandbox/contracts/defaultImpl/WSReceiver.sol
+++ smartcontracts/sandbox/contracts/defaultImpl/WSReceiver.sol
@@ -1,27 +1,28 @@
 // SPDX-License-Identifier: MIT
 pragma solidity ^0.8.19;
 
-import {IWSReceiver} from "../interfaces/IWSReceiver.sol";
+import {IWSReceiver} from "../interfaces/IReceiver.sol";
 
 contract WSReceiver is IWSReceiver {
     address public deviceNFTRegistry;
 
-    mapping(uint256 => Tunnel) public tunnels;
+    uint256 currentBatchHeight;
+    mapping(uint256 => Batch) batches;
 
     constructor(address _deviceNFTRegistry) {
         deviceNFTRegistry = _deviceNFTRegistry;
     }
 
-    function receiveData(uint256 _tunnelId, bytes32 _batchMR, bytes32 _devicesMR, bytes calldata _zkProof) external {
+    function receiveData(bytes32 _batchMR, bytes32 _devicesMR, bytes calldata _zkProof) external {
         _verify(_zkProof);
 
-        uint256 newHeight = tunnels[_tunnelId].currentBatchHeight + 1;
-        tunnels[_tunnelId].batches[tunnels[_tunnelId].currentBatchHeight] = Batch(_batchMR, _devicesMR);
-        tunnels[_tunnelId].currentBatchHeight = newHeight;
+        uint256 newHeight = currentBatchHeight + 1;
+        batches[currentBatchHeight] = Batch(_batchMR, _devicesMR);
+        currentBatchHeight = newHeight;
     }
 
-    function getBatchHeight(uint256 _tunnelId) external view returns (uint256) {
-        return tunnels[_tunnelId].currentBatchHeight;
+    function getBatchHeight() external view returns (uint256) {
+        return currentBatchHeight;
     }
 
     function _verify(bytes calldata) internal pure returns (bool) {
--- smartcontracts/sandbox/contracts/interfaces/IReceiver.sol
+++ smartcontracts/sandbox/contracts/interfaces/IReceiver.sol
@@ -3,11 +3,6 @@ pragma solidity ^0.8.19;
 
 /// @title IWSReceiver
 interface IWSReceiver {
-    struct Tunnel {
-        uint256 currentBatchHeight;
-        mapping(uint256 => Batch) batches;
-    }
-
     struct Batch {
         bytes32 merkleRoot;
         bytes32 devicesMerkleRoot;
@@ -18,12 +13,11 @@ interface IWSReceiver {
     function deviceNFTRegistry() external view returns (address);
 
     /// @notice receive data to process from WSRouter
-    /// @param _tunnelId tunnel id
     /// @param _batchMR merkle root of the batch
     /// @param _devicesMR merkle root of the devices
     /// @param _zkProof zk proof
-    function receiveData(uint256 _tunnelId, bytes32 _batchMR, bytes32 _devicesMR, bytes calldata _zkProof) external;
+    function receiveData(bytes32 _batchMR, bytes32 _devicesMR, bytes calldata _zkProof) external;
 
     /// @notice get batch height
-    function getBatchHeight(uint256 _tunnelId) external view returns (uint256);
+    function getBatchHeight() external view returns (uint256);
 }
