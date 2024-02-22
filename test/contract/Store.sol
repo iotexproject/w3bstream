52933cdd20aea01f0fe85dec8e70b5000719a610
huangzhiran <zhiran@iotex.me>
2024-02-22 17:28:48 +0800

    feat(output): support snark param

--- test/contract/Store.sol
+++ test/contract/Store.sol
@@ -7,6 +7,10 @@ contract Store {
         proof = _proof;
     }
 
+    function setProofSnark(bytes calldata proof_snark_seal, bytes calldata proof_snark_post_state_digest, bytes calldata proof_snark_journal) public {
+        proof = proof_snark_seal;
+    }
+
     function getProof() public view returns (bytes memory){
         return proof;
     }
