1a7bee11a5583cc2188b6dc2c2e5f06ab00a4c0c
Nicky Ru <64008830+nicky-ru@users.noreply.github.com>
2024-02-27 01:33:52 +1000

    Update WSReceiver.sol

--- smartcontracts/sandbox/contracts/defaultImpl/WSReceiver.sol
+++ smartcontracts/sandbox/contracts/defaultImpl/WSReceiver.sol
@@ -1,27 +1,50 @@
 // SPDX-License-Identifier: MIT
 pragma solidity ^0.8.19;
 
+import {IERC721} from "@openzeppelin/contracts/token/ERC721/IERC721.sol";
+import {IERC20} from "@openzeppelin/contracts/token/ERC20/IERC20.sol";
+
 import {IWSReceiver} from "../interfaces/IWSReceiver.sol";
+import {IDeviceRegistry} from "../interfaces/IDeviceRegistry.sol";
+import {IDeviceReward} from "../interfaces/IDeviceReward.sol";
+
+import "../lib/JournalParser.sol";
 
 contract WSReceiver is IWSReceiver {
     address public deviceNFTRegistry;
+    address public tokenAddress;
 
     uint256 currentBatchHeight;
     mapping(uint256 => Batch) batches;
 
-    constructor(address _deviceNFTRegistry) {
+    constructor(address _deviceNFTRegistry, address _tokenAddress) {
         deviceNFTRegistry = _deviceNFTRegistry;
+        tokenAddress = _tokenAddress;
     }
 
+    error DeviceIsNotEnabled(uint256);
+
     function receiveData(bytes calldata _data) external {
-        _verify(_data[64:]);
+        (
+            bytes memory proof_snark_seal,
+            bytes memory proof_snark_post_state_digest,
+            bytes memory proof_snark_journal
+        ) = abi.decode(_data, (bytes, bytes, bytes));
 
-        bytes32 _batchMR = bytes32(_data[:32]);
-        bytes32 _devicesMR = bytes32(_data[32:64]);
+        bytes memory devicesJsonString = JournalParser.byteStringToBytes(proof_snark_journal);
 
-        uint256 newHeight = currentBatchHeight + 1;
-        batches[currentBatchHeight] = Batch(_batchMR, _devicesMR);
-        currentBatchHeight = newHeight;
+        (JournalParser.Device[] memory devices, uint256 devicesLen) = JournalParser.parseDeviceJson(devicesJsonString);
+
+        for (uint256 i; i < devicesLen; i++) {
+            uint256 deviceId = devices[i].id;
+            IDeviceRegistry.Device memory device = IDeviceRegistry(deviceNFTRegistry).getDeviceInfo(deviceId);
+            if (!device.enabled) {
+                revert DeviceIsNotEnabled(devices[i].id);
+            }
+
+            address owner = IERC721(deviceNFTRegistry).ownerOf(deviceId);
+            IDeviceReward(tokenAddress).mint(owner, devices[i].reward);
+        }
     }
 
     function getBatchHeight() external view returns (uint256) {
