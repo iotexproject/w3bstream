3fb9e7c64733d23b2e8fb4a08026c291f30c06d7
Nicky Ru <64008830+nicky-ru@users.noreply.github.com>
2024-02-27 01:34:51 +1000

    add device rewards

---
+++ smartcontracts/sandbox/contracts/defaultImpl/DeviceReward.sol
@@ -0,0 +1,18 @@
+// SPDX-License-Identifier: MIT
+pragma solidity ^0.8.19;
+
+import "@openzeppelin/contracts/token/ERC20/ERC20.sol";
+import "@openzeppelin/contracts/access/AccessControl.sol";
+
+contract DeviceReward is ERC20, AccessControl {
+    bytes32 public constant MINTER_ROLE = keccak256("MINTER_ROLE");
+
+    constructor() ERC20("DeviceReward", "DRW") {
+        _grantRole(DEFAULT_ADMIN_ROLE, msg.sender);
+        _grantRole(MINTER_ROLE, msg.sender);
+    }
+
+    function mint(address to, uint256 amount) public onlyRole(MINTER_ROLE) {
+        _mint(to, amount);
+    }
+}
---
+++ smartcontracts/sandbox/contracts/interfaces/IDeviceReward.sol
@@ -0,0 +1,9 @@
+// SPDX-License-Identifier: MIT
+pragma solidity ^0.8.19;
+
+import "@openzeppelin/contracts/token/ERC20/ERC20.sol";
+import "@openzeppelin/contracts/access/AccessControl.sol";
+
+interface IDeviceReward {
+    function mint(address to, uint256 amount) external;
+}
