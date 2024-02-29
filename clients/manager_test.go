500d1846628850aed09b18ede727b1894ed86225
saito <birdyfj@gmail.com>
2024-02-29 21:57:41 +0800

    test(clients): add unit tests (#311)
    * test(clients): add unit tests

---
+++ clients/manager_test.go
@@ -0,0 +1,57 @@
+package clients_test
+
+import (
+	"fmt"
+	"github.com/agiledragon/gomonkey/v2"
+	"github.com/machinefi/sprout/clients"
+	"github.com/machinefi/sprout/testutil"
+	"github.com/pkg/errors"
+	"github.com/stretchr/testify/require"
+	"testing"
+)
+
+func TestClientManager(t *testing.T) {
+	var (
+		r = require.New(t)
+		p = gomonkey.NewPatches()
+		m *clients.Manager
+	)
+
+	t.Run("FailedToFillMockClients", func(t *testing.T) {
+		p = testutil.JsonUnmarshal(p, errors.New(t.Name()))
+
+		defer p.Reset()
+		defer func() {
+			if err := recover(); err != nil {
+				r.Contains(fmt.Sprint(err), t.Name())
+			}
+		}()
+
+		_ = clients.NewManager()
+	})
+
+	t.Run("NewManager", func(t *testing.T) {
+		m = clients.NewManager()
+		r.NotNil(m)
+		r.Equal(m, clients.NewManager())
+	})
+
+	t.Run("AddAndGetClient", func(t *testing.T) {
+		c, ok := m.GetByClientDID("did:ethr:0x9d9250fb4e08ba7a858fe7196a6ba946c6083ff0")
+		r.NotNil(c)
+		r.True(ok)
+		c, ok = m.GetByClientDID("not_exists")
+		r.Nil(c)
+		r.False(ok)
+
+		m.AddClient(&clients.Client{
+			ClientDID: "unit_test_added",
+			Projects:  []uint64{1, 2, 3},
+		})
+		c, ok = m.GetByClientDID("unit_test_added")
+		r.NotNil(c)
+		r.True(ok)
+		r.NotNil(c.Metadata)
+		r.Len(c.Projects, 3)
+	})
+}
