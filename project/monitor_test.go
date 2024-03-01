5e7425113aec5699655d9a373f026db28328648a
huangzhiran <zhiran@iotex.me>
2024-03-01 15:31:41 +0800

    test(project) add unit test for project monitor method

--- project/monitor_test.go
+++ project/monitor_test.go
@@ -5,6 +5,9 @@ import (
 	"time"
 
 	. "github.com/bytedance/mockey"
+	"github.com/ethereum/go-ethereum/common"
+	"github.com/ethereum/go-ethereum/core/types"
+	"github.com/ethereum/go-ethereum/crypto"
 	"github.com/ethereum/go-ethereum/ethclient"
 	"github.com/pkg/errors"
 	. "github.com/smartystreets/goconvey/convey"
@@ -45,5 +48,112 @@ func TestNewMonitor(t *testing.T) {
 
 		_, err := NewDefaultMonitor("", []string{}, []string{})
 		So(err, ShouldBeEmpty)
+	})
+}
+
+func TestMonitorMethod(t *testing.T) {
+	Convey("Err", t, func() {
+		m := &Monitor{
+			err: make(chan error, 1),
+		}
+		res := m.Err()
+		err := errors.New(t.Name())
+		m.err <- err
+		So(<-res, ShouldEqual, err)
+	})
+	Convey("Unsubscribe", t, func() {
+		m := &Monitor{
+			stop: make(chan struct{}, 1),
+		}
+		m.Unsubscribe()
+		So(<-m.stop, ShouldNotBeEmpty)
+	})
+	Convey("Events", t, func() {
+		topic := "test"
+		m := &Monitor{
+			events: map[common.Hash]chan *types.Log{crypto.Keccak256Hash([]byte(topic)): make(chan *types.Log)},
+		}
+		_, ok := m.Events(topic)
+		So(ok, ShouldBeTrue)
+	})
+	Convey("MustEvents", t, func() {
+		topic := "test"
+		m := &Monitor{
+			events: map[common.Hash]chan *types.Log{crypto.Keccak256Hash([]byte(topic)): make(chan *types.Log)},
+		}
+		ch := m.MustEvents(topic)
+		So(ch, ShouldNotBeNil)
+	})
+	Convey("DoRunStopped", t, func() {
+		m := &Monitor{
+			stop: make(chan struct{}, 1),
+		}
+		m.Unsubscribe()
+		finished := m.doRun()
+		So(finished, ShouldBeTrue)
+	})
+	PatchConvey("DoRunBlockNumberFailed", t, func() {
+		Mock((*ethclient.Client).BlockNumber).Return(uint64(100), errors.New(t.Name())).Build()
+		Mock(time.Sleep).Return().Build()
+
+		m := &Monitor{
+			stop: make(chan struct{}, 1),
+		}
+		finished := m.doRun()
+		So(finished, ShouldBeFalse)
+	})
+	PatchConvey("DoRunBlockNumberBehind", t, func() {
+		Mock((*ethclient.Client).BlockNumber).Return(uint64(100), nil).Build()
+		Mock(time.Sleep).Return().Build()
+
+		m := &Monitor{
+			latest: 1000,
+			stop:   make(chan struct{}, 1),
+		}
+		finished := m.doRun()
+		So(finished, ShouldBeFalse)
+	})
+	PatchConvey("DoRunFilterLogsFailed", t, func() {
+		Mock((*ethclient.Client).BlockNumber).Return(uint64(100), nil).Build()
+		Mock((*ethclient.Client).FilterLogs).Return(nil, errors.New(t.Name())).Build()
+		Mock(time.Sleep).Return().Build()
+
+		m := &Monitor{
+			latest: 1,
+			stop:   make(chan struct{}, 1),
+		}
+		finished := m.doRun()
+		So(finished, ShouldBeFalse)
+	})
+	PatchConvey("DoRunFilterLogsEmpty", t, func() {
+		Mock((*ethclient.Client).BlockNumber).Return(uint64(100), nil).Build()
+		Mock((*ethclient.Client).FilterLogs).Return(nil, nil).Build()
+		Mock(time.Sleep).Return().Build()
+
+		m := &Monitor{
+			latest: 1,
+			stop:   make(chan struct{}, 1),
+		}
+		finished := m.doRun()
+		So(finished, ShouldBeFalse)
+	})
+	PatchConvey("DoRunFilterLogsEmpty", t, func() {
+		Mock((*ethclient.Client).BlockNumber).Return(uint64(100), nil).Build()
+		Mock((*ethclient.Client).FilterLogs).Return([]types.Log{{Topics: []common.Hash{crypto.Keccak256Hash([]byte("0"))}}}, nil).Build()
+		Mock(time.Sleep).Return().Build()
+
+		m := &Monitor{
+			latest: 1,
+			stop:   make(chan struct{}, 1),
+		}
+		finished := m.doRun()
+		So(finished, ShouldBeFalse)
+	})
+	PatchConvey("Run", t, func() {
+		moker := Mock((*Monitor).doRun).Return(true).Build()
+
+		m := &Monitor{}
+		m.run()
+		So(moker.Times(), ShouldEqual, 1)
 	})
 }
