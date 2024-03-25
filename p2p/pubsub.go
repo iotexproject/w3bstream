1b5a454a98d0fa18ac30f6334b7f20f2f9d02218
saito <birdyfj@gmail.com>
2024-03-25 09:06:24 +0800

    feat(p2p): optimize p2p subscriber (#365)

--- p2p/pubsub.go
+++ p2p/pubsub.go
@@ -4,138 +4,13 @@ import (
 	"context"
 	"log/slog"
 	"strconv"
-	"sync"
 
-	"github.com/libp2p/go-libp2p"
 	pubsub "github.com/libp2p/go-libp2p-pubsub"
 	"github.com/libp2p/go-libp2p/core/peer"
-	"github.com/libp2p/go-libp2p/p2p/muxer/yamux"
 	"github.com/pkg/errors"
 )
 
-type HandleSubscriptionMessage func([]byte, *pubsub.Topic)
-
-type PubSubs struct {
-	mux     sync.RWMutex
-	pubSubs map[uint64]*pubSub
-	ps      *pubsub.PubSub
-	selfID  peer.ID
-	handle  HandleSubscriptionMessage
-}
-
-func (p *PubSubs) Add(projectID uint64) error {
-	p.mux.Lock()
-	defer p.mux.Unlock()
-
-	if _, ok := p.pubSubs[projectID]; ok {
-		return nil
-	}
-
-	nps, err := NewPubSub(projectID, p.ps, p.handle, p.selfID)
-	if err != nil {
-		return err
-	}
-	slog.With("project", projectID).Info("subscribe started")
-	go nps.run()
-
-	p.pubSubs[projectID] = nps
-	return nil
-}
-
-// TODO delete not used currently
-func (p *PubSubs) Delete(projectID uint64) {
-	p.mux.Lock()
-	defer p.mux.Unlock()
-
-	pubSub, ok := p.pubSubs[projectID]
-	if !ok {
-		return
-	}
-	pubSub.release()
-	delete(p.pubSubs, projectID)
-}
-
-func (p *PubSubs) Publish(projectID uint64, d []byte) error {
-	p.mux.RLock()
-	defer p.mux.RUnlock()
-
-	s, ok := p.pubSubs[projectID]
-	if !ok {
-		return errors.Errorf("project %v topic not exist", projectID)
-	}
-	if err := s.topic.Publish(context.Background(), d); err != nil {
-		return errors.Wrap(err, "failed to publish data to p2p network")
-	}
-	return nil
-}
-
-func NewPubSubs(handle HandleSubscriptionMessage, bootNodeMultiaddr string, iotexChainID int) (*PubSubs, error) {
-	ctx := context.Background()
-	h, err := libp2p.New(libp2p.ListenAddrStrings("/ip4/0.0.0.0/tcp/0"), libp2p.Muxer("/yamux/2.0.0", yamux.DefaultTransport))
-	if err != nil {
-		return nil, errors.Wrap(err, "new libp2p host failed")
-	}
-
-	ps, err := pubsub.NewGossipSub(ctx, h, pubsub.WithMaxMessageSize(2*pubsub.DefaultMaxMessageSize))
-	if err != nil {
-		return nil, errors.Wrap(err, "new gossip subscription failed")
-	}
-	if err := discoverPeers(ctx, h, bootNodeMultiaddr, iotexChainID); err != nil {
-		return nil, err
-	}
-
-	return &PubSubs{
-		ps:      ps,
-		pubSubs: make(map[uint64]*pubSub),
-		selfID:  h.ID(),
-		handle:  handle,
-	}, nil
-}
-
-type pubSub struct {
-	selfID       peer.ID
-	topic        *pubsub.Topic
-	subscription *pubsub.Subscription
-	handle       HandleSubscriptionMessage
-	ctx          context.Context
-	ctxCancel    context.CancelFunc
-}
-
-func (p *pubSub) release() {
-	p.ctxCancel()
-	p.subscription.Cancel()
-	if err := p.topic.Close(); err != nil {
-		slog.Error("failed to close topic", "error", err, "topic", p.topic.String())
-	}
-}
-
-func (p *pubSub) run() {
-	for {
-		select {
-		case <-p.ctx.Done():
-			slog.With("ctx.Err()", p.ctx.Err()).Info("pubsub stopped caused by")
-			return
-		default:
-			if err := p.nextMsg(); err != nil {
-				slog.Error("failed to get pubsub msg", "error", err)
-			}
-		}
-	}
-}
-
-func (p *pubSub) nextMsg() error {
-	m, err := p.subscription.Next(p.ctx)
-	if err != nil {
-		return errors.Wrapf(err, "failed to get p2p data")
-	}
-	if m.ReceivedFrom == p.selfID {
-		return nil
-	}
-	p.handle(m.Message.Data, p.topic)
-	return nil
-}
-
-func NewPubSub(projectID uint64, ps *pubsub.PubSub, handle HandleSubscriptionMessage, selfID peer.ID) (*pubSub, error) {
+func newSubscriber(projectID uint64, ps *pubsub.PubSub, handle HandleSubscriptionMessage, selfID peer.ID) (*subscriber, error) {
 	topic, err := ps.Join("w3bstream-project-" + strconv.FormatUint(projectID, 10))
 	if err != nil {
 		return nil, errors.Wrapf(err, "join topic %v failed", projectID)
@@ -146,12 +21,54 @@ func NewPubSub(projectID uint64, ps *pubsub.PubSub, handle HandleSubscriptionMes
 	}
 	ctx, cancel := context.WithCancel(context.Background())
 
-	return &pubSub{
+	_ps := &subscriber{
 		selfID:       selfID,
 		topic:        topic,
 		subscription: sub,
 		handle:       handle,
-		ctx:          ctx,
-		ctxCancel:    cancel,
-	}, nil
+		cancel:       cancel,
+	}
+
+	go _ps.run(ctx)
+	slog.With("project", projectID).Info("subscribe started")
+
+	return _ps, nil
+}
+
+type subscriber struct {
+	selfID       peer.ID
+	topic        *pubsub.Topic
+	subscription *pubsub.Subscription
+	handle       HandleSubscriptionMessage
+	cancel       context.CancelFunc
+}
+
+func (p *subscriber) release() {
+	p.subscription.Cancel()
+	p.cancel()
+	if err := p.topic.Close(); err != nil {
+		slog.Error("failed to close topic", "error", err, "topic", p.topic.String())
+	}
+}
+
+func (p *subscriber) run(ctx context.Context) {
+	for {
+		select {
+		case <-ctx.Done():
+			slog.With("ctx.Err()", ctx.Err()).Info("pubsub stopped caused by")
+			return
+		default:
+			// p.handle([]byte(""), p.topic)
+			m, err := p.subscription.Next(ctx)
+			if err != nil {
+				slog.Error("failed to get p2p data", "error", err)
+				continue
+			}
+			if m.ReceivedFrom == p.selfID {
+				slog.Info("skip message from self")
+				continue
+			}
+			p.handle(m.Message.Data, p.topic)
+		}
+	}
 }
