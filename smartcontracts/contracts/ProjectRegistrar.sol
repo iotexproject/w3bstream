ec02fbe9fb301de9e5aa86baff7ef09779104bca
ququzone <xueping.yang@gmail.com>
2024-03-28 23:50:48 +0800

    merge d5b1f69d commit

--- smartcontracts/contracts/ProjectRegistrar.sol
+++ smartcontracts/contracts/ProjectRegistrar.sol
@@ -9,71 +9,110 @@ import {ERC721Upgradeable} from "@openzeppelin/contracts-upgradeable/token/ERC72
 contract ProjectRegistrar is IProjectRegistrar, OwnableUpgradeable, ERC721Upgradeable {
     event FeeWithdrawn(address indexed account, uint256 amount);
 
-    uint256 public override fee;
+    uint256 public override registrationFee;
+    mapping(uint256 => mapping(address => bool)) public override operators;
+    mapping(uint256 => mapping(bytes32 => bytes)) public override attributes;
 
     mapping(uint256 => Project) projects;
     uint256 nextProjectId;
 
     modifier onlyProjectOwner(uint256 _projectId) {
-        require(ownerOf(_projectId) == msg.sender, "Only project owner to operate");
+        require(ownerOf(_projectId) == msg.sender, "not owner");
         _;
     }
 
-    function _requireOperator(address _operator) internal view virtual {
-        require(_operator == msg.sender, "Only project operator to operate");
+    modifier onlyProjectOperator(uint256 _projectId) {
+        require(ownerOf(_projectId) == msg.sender || operators[_projectId][msg.sender], "not operator");
+        _;
     }
 
-    function initialize(uint256 _fee, string calldata _name, string calldata _symbol) public initializer {
+    function initialize(uint256 _registrationFee, string calldata _name, string calldata _symbol) public initializer {
         __Ownable_init();
         __ERC721_init(_name, _symbol);
-        fee = _fee;
+        registrationFee = _registrationFee;
+        emit RegistrationFeeSet(_registrationFee);
     }
 
     function isPaused(uint256 _projectId) external view override returns (bool) {
+        _requireMinted(_projectId);
         return projects[_projectId].paused;
     }
 
-    function operator(uint256 _projectId) external view override returns (address) {
-        return projects[_projectId].operator;
-    }
-
     function hash(uint256 _projectId) external view override returns (bytes32) {
+        _requireMinted(_projectId);
         return projects[_projectId].hash;
     }
 
     function uri(uint256 _projectId) external view override returns (string memory) {
+        _requireMinted(_projectId);
         return projects[_projectId].uri;
     }
 
-    function metadata(uint256 _projectId, bytes32 _name) external view override returns (bytes memory) {
-        return projects[_projectId].metadata[_name];
+    function attributesOf(
+        uint256 _projectId,
+        bytes32[] memory _keys
+    ) external view override returns (bytes[] memory values_) {
+        _requireMinted(_projectId);
+
+        values_ = new bytes[](_keys.length);
+        mapping(bytes32 => bytes) storage project = attributes[_projectId];
+        for (uint i = 0; i < _keys.length; i++) {
+            values_[i] = project[_keys[i]];
+        }
     }
 
     function register(string calldata _uri, bytes32 _hash) external payable override returns (uint256 _projectId) {
-        require(msg.value > fee, "Register fee too low");
+        require(msg.value > registrationFee, "insufficient fee");
         _projectId = ++nextProjectId;
 
         Project storage _project = projects[_projectId];
         _project.paused = false;
-        _project.operator = msg.sender;
         _project.hash = _hash;
         _project.uri = _uri;
 
         _mint(msg.sender, _projectId);
-        emit ProjectCreated(msg.sender, _projectId);
+        emit ProjectCreated(msg.sender, _projectId, _uri, _hash);
     }
 
-    function changeOperator(uint256 _projectId, address _operator) external override onlyProjectOwner(_projectId) {
-        require(_operator != address(0), "zero address");
-        projects[_projectId].operator = _operator;
+    function setAttributes(
+        uint64 _projectId,
+        bytes32[] memory _keys,
+        bytes[] memory _values
+    ) external onlyProjectOperator(_projectId) {
+        require(_keys.length == _values.length, "invalid input");
 
-        emit ProjectOperatorChanged(_projectId, _operator);
+        mapping(bytes32 => bytes) storage _attributes = attributes[_projectId];
+        for (uint i = 0; i < _keys.length; i++) {
+            _attributes[_keys[i]] = _values[i];
+            emit AttributeSet(_projectId, _keys[i], _values[i]);
+        }
     }
 
-    function pause(uint256 _projectId) external override {
-        _requireMinted(_projectId);
+    function addOperator(uint256 _projectId, address _operator) external override onlyProjectOwner(_projectId) {
+        operators[_projectId][_operator] = true;
+        emit OperatorAdded(_projectId, _operator);
+    }
+
+    function removeOperator(uint256 _projectId, address _operator) external override onlyProjectOwner(_projectId) {
+        operators[_projectId][_operator] = false;
+        emit OperatorRemoved(_projectId, _operator);
+    }
+
+    function updateProject(
+        uint256 _projectId,
+        string memory _uri,
+        bytes32 _hash
+    ) external override onlyProjectOwner(_projectId) {
+        require(bytes(_uri).length != 0, "empty uri");
+        Project storage project = projects[_projectId];
+        project.uri = _uri;
+        project.hash = _hash;
+
+        emit ProjectUpdated(_projectId, _uri, _hash);
+    }
+
+    function pause(uint256 _projectId) external override onlyProjectOperator(_projectId) {
         Project storage _project = projects[_projectId];
-        _requireOperator(_project.operator);
         require(!_project.paused, "project already paused");
 
         _project.paused = true;
@@ -81,44 +120,13 @@ contract ProjectRegistrar is IProjectRegistrar, OwnableUpgradeable, ERC721Upgrad
         emit ProjectPaused(_projectId);
     }
 
-    function resume(uint256 _projectId) external override {
-        _requireMinted(_projectId);
+    function resume(uint256 _projectId) external override onlyProjectOperator(_projectId) {
         Project storage _project = projects[_projectId];
-        _requireOperator(_project.operator);
         require(_project.paused, "project already actived");
 
         _project.paused = false;
 
         emit ProjectResumed(_projectId);
-    }
-
-    function updateConfig(uint256 _projectId, string calldata _uri, bytes32 _hash) external override {
-        _requireMinted(_projectId);
-        Project storage _project = projects[_projectId];
-        _requireOperator(_project.operator);
-        _project.hash = _hash;
-        _project.uri = _uri;
-
-        emit ProjectConfigUpdated(_projectId);
-    }
-
-    function updateMetadata(uint256 _projectId, bytes32 _name, bytes calldata _value) external override {
-        _requireMinted(_projectId);
-        Project storage _project = projects[_projectId];
-        _requireOperator(_project.operator);
-        _project.metadata[_name] = _value;
-
-        emit ProjectMetadataUpdated(_projectId, _name);
-    }
-
-    function removeMetadata(uint256 _projectId, bytes32 _name) external override {
-        _requireMinted(_projectId);
-        Project storage _project = projects[_projectId];
-        _requireOperator(_project.operator);
-
-        delete _project.metadata[_name];
-
-        emit ProjectMetadataRemoved(_projectId, _name);
     }
 
     function withdrawFee(address _account, uint256 _amount) external onlyOwner {
--- smartcontracts/contracts/interfaces/IProjectRegistrar.sol
+++ smartcontracts/contracts/interfaces/IProjectRegistrar.sol
@@ -3,36 +3,34 @@ pragma solidity ^0.8.19;
 
 interface IProjectRegistrar {
     struct Project {
-        bool paused;
-        address operator;
-        bytes32 hash;
         string uri;
-        mapping(bytes32 => bytes) metadata;
+        bytes32 hash;
+        bool paused;
     }
+    event RegistrationFeeSet(uint256 fee);
 
-    event FeeChanged(uint256 indexed fee);
-
-    event ProjectCreated(address indexed owner, uint256 indexed projectId);
-    event ProjectOperatorChanged(uint256 indexed projectId, address indexed operator);
+    event ProjectCreated(address indexed owner, uint256 indexed projectId, string uri, bytes32 hash);
+    event AttributeSet(uint256 indexed projectId, bytes32 indexed key, bytes value);
+    event AttributeRemoved(uint256 indexed projectId, bytes32 indexed key);
+    event OperatorAdded(uint256 indexed projectId, address indexed operator);
+    event OperatorRemoved(uint256 indexed projectId, address indexed operator);
     event ProjectPaused(uint256 indexed projectId);
     event ProjectResumed(uint256 indexed projectId);
-    event ProjectConfigUpdated(uint256 indexed projectId);
-    event ProjectMetadataUpdated(uint256 indexed projectId, bytes32 name);
-    event ProjectMetadataRemoved(uint256 indexed projectId, bytes32 name);
+    event ProjectUpdated(uint256 indexed projectId, string uri, bytes32 hash);
 
-    function fee() external view returns (uint256);
+    function registrationFee() external view returns (uint256);
 
     function isPaused(uint256 _projectId) external view returns (bool);
-    function operator(uint256 _projectId) external view returns (address);
     function hash(uint256 _projectId) external view returns (bytes32);
     function uri(uint256 _projectId) external view returns (string calldata);
-    function metadata(uint256 _projectId, bytes32 _name) external view returns (bytes calldata);
+    function operators(uint256 _projectId, address _operator) external view returns (bool);
+    function attributes(uint256 _projectId, bytes32 _name) external view returns (bytes calldata);
+    function attributesOf(uint256 _projectId, bytes32[] calldata _keys) external view returns (bytes[] calldata);
 
-    function register(string calldata _uri, bytes32 _hash) external payable returns (uint256 _proejctId);
-    function changeOperator(uint256 _projectId, address _operator) external;
+    function register(string calldata _uri, bytes32 _hash) external payable returns (uint256 _external);
     function pause(uint256 _projectId) external;
     function resume(uint256 _projectId) external;
-    function updateConfig(uint256 _projectId, string calldata _uri, bytes32 _hash) external;
-    function updateMetadata(uint256 _projectId, bytes32 _name, bytes calldata _value) external;
-    function removeMetadata(uint256 _projectId, bytes32 _name) external;
+    function addOperator(uint256 _projectId, address _operator) external;
+    function removeOperator(uint256 _projectId, address _operator) external;
+    function updateProject(uint256 _projectId, string memory _uri, bytes32 _hash) external;
 }
