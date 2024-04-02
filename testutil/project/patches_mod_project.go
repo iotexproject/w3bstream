061e100bad456c3a8a1917bafcc3dd8466c19701
sincos <birdyfj@gmail.com>
2024-04-02 10:17:27 +0800

    chore: fix unit test

--- testutil/project/patches_mod_project.go
+++ testutil/project/patches_mod_project.go
@@ -8,12 +8,12 @@ import (
 	"github.com/machinefi/sprout/project"
 )
 
-func ProjectConfigManagerGet(p *Patches, conf *project.Config, err error) *Patches {
+func ProjectConfigManagerGet(p *Patches, conf *project.ConfigData, err error) *Patches {
 	var pm *project.ConfigManager
 	return p.ApplyMethodFunc(
 		reflect.TypeOf(pm),
 		"Get",
-		func(projectID uint64, version string) (*project.Config, error) {
+		func(projectID uint64, version string) (*project.ConfigData, error) {
 			return conf, err
 		},
 	)
