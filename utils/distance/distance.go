29361e25f3929e62b55ff36701003bb5aa79409a
sincos <birdyfj@gmail.com>
2024-04-03 14:47:53 +0800

    feat(utils/distance): add Point and Points type for cal distance

--- utils/distance/distance.go
+++ utils/distance/distance.go
@@ -1,7 +1,10 @@
 package distance
 
 import (
+	"bytes"
 	"crypto/sha256"
+	"encoding/binary"
+	"log/slog"
 	"math/big"
 	"sort"
 
@@ -40,3 +43,59 @@ func GetMinNLocation(locations []string, myLocation, n uint64) []string {
 	}
 	return result
 }
+
+func NewPoint(val any) (*Point, error) {
+	var (
+		buf = bytes.NewBuffer([]byte{})
+		err error
+	)
+
+	switch v := val.(type) {
+	case string:
+		_, err = buf.WriteString(v)
+	case []byte:
+		_, err = buf.Write(v)
+	default:
+		err = binary.Write(buf, binary.LittleEndian, v)
+	}
+	if err != nil {
+		return nil, err
+	}
+	h := sha256.Sum256(buf.Bytes())
+	return &Point{
+		value: val,
+		point: new(big.Int).SetBytes(h[:]),
+	}, nil
+}
+
+func MustNewPoint(val any) *Point {
+	point, err := NewPoint(val)
+	if err != nil {
+		slog.Error("failed to new pointer", "value", val, "error", err)
+		panic(err)
+	}
+	return point
+}
+
+type Point struct {
+	value any
+	point *big.Int
+}
+
+func (p *Point) Distance(other *Point) *big.Int {
+	return new(big.Int).Xor(p.point, other.point)
+}
+
+func (p *Point) Value() any {
+	return p.value
+}
+
+func NewPoints(values ...any) Points {
+	points := make(Points, 0, len(values))
+	for i := range values {
+		points = append(points, MustNewPoint(values[i]))
+	}
+	return points
+}
+
+type Points []*Point
