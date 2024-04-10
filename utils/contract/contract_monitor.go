8ae2b3828b0440713e7e0a0e2c99c2d9d35846c0
hunshenshi <289517357@qq.com>
2024-04-10 20:54:35 +0800

    feat(monitor): monitor more events

--- utils/contract/contract_monitor.go
+++ utils/contract/contract_monitor.go
@@ -19,20 +19,34 @@ import (
 )
 
 const (
-	ProjectConfigUpdatedTopic = "ProjectConfigUpdated(uint256,string,bytes32)"
-	OperatorSetTopic          = "OperatorSet(uint256,address)"
+	RequiredProverAmount = "RequiredProverAmount"
+	VmType               = "VmType"
+
+	attributeSetTopic         = "AttributeSet(uint256,bytes32,bytes)"
+	projectPausedTopic        = "ProjectPaused(uint256)"
+	projectResumedTopic       = "ProjectResumed(uint256)"
+	projectConfigUpdatedTopic = "ProjectConfigUpdated(uint256,string,bytes32)"
+
+	operatorSetTopic     = "OperatorSet(uint256,address)"
+	nodeTypeUpdatedTopic = "NodeTypeUpdated(uint256,uint256)"
+	proverPausedTopic    = "ProverPaused(uint256)"
+	proverResumedTopic   = "ProverResumed(uint256)"
 )
 
 type Project struct {
-	Uri         string
-	Hash        [32]byte
 	ID          uint64
 	BlockNumber uint64
+	Paused      *bool
+	Uri         string
+	Hash        [32]byte
+	Attributes  map[[32]byte][]byte
 }
 
 type Prover struct {
 	OperatorAddress string
 	BlockNumber     uint64
+	Paused          *bool
+	NodeTypes       uint64
 }
 
 func ListAndWatchProject(chainEndpoint, contractAddress string) (<-chan *Project, error) {
@@ -51,7 +65,8 @@ func ListAndWatchProject(chainEndpoint, contractAddress string) (<-chan *Project
 	if err != nil {
 		return nil, errors.Wrap(err, "failed to query the latest block number")
 	}
-	watchProject(ch, client, instance, 3*time.Second, contractAddress, ProjectConfigUpdatedTopic, 1000, latestBlockNumber)
+	watchProject(ch, client, instance, 3*time.Second, contractAddress,
+		[]string{attributeSetTopic, projectPausedTopic, projectResumedTopic, projectConfigUpdatedTopic}, 1000, latestBlockNumber)
 	if err := listProject(ch, instance, latestBlockNumber); err != nil {
 		return nil, err
 	}
@@ -68,20 +83,48 @@ func listProject(ch chan<- *Project, instance *project.Project, targetBlockNumbe
 		if mp.Uri == "" || bytes.Equal(mp.Hash[:], emptyHash[:]) {
 			return nil
 		}
+
+		isPaused, err := instance.IsPaused(&bind.CallOpts{}, new(big.Int).SetUint64(projectID))
+		if err != nil {
+			return errors.Wrapf(err, "failed to get project pause status from chain, project_id %v", projectID)
+		}
+
+		proverAmt, err := instance.Attributes(&bind.CallOpts{}, new(big.Int).SetUint64(projectID),
+			crypto.Keccak256Hash([]byte(RequiredProverAmount)))
+		if err != nil {
+			return errors.Wrapf(err, "failed to get project attributes from chain, project_id %v, key %s", projectID, RequiredProverAmount)
+		}
+		vmType, err := instance.Attributes(&bind.CallOpts{}, new(big.Int).SetUint64(projectID),
+			crypto.Keccak256Hash([]byte(VmType)))
+		if err != nil {
+			return errors.Wrapf(err, "failed to get project attributes from chain, project_id %v, key %s", projectID, vmType)
+		}
+		attributes := make(map[[32]byte][]byte)
+		attributes[crypto.Keccak256Hash([]byte(RequiredProverAmount))] = proverAmt
+		attributes[crypto.Keccak256Hash([]byte(VmType))] = vmType
+
 		ch <- &Project{
-			Uri:         mp.Uri,
-			Hash:        mp.Hash,
 			ID:          projectID,
 			BlockNumber: targetBlockNumber,
+			Paused:      &isPaused,
+			Uri:         mp.Uri,
+			Hash:        mp.Hash,
+			Attributes:  attributes,
 		}
 	}
 }
 
-func watchProject(ch chan<- *Project, client *ethclient.Client, instance *project.Project, interval time.Duration, contractAddress, topic string, step, startBlockNumber uint64) {
+func watchProject(ch chan<- *Project, client *ethclient.Client, instance *project.Project, interval time.Duration,
+	contractAddress string, topics []string, step, startBlockNumber uint64) {
 	queriedBlockNumber := startBlockNumber
 	query := ethereum.FilterQuery{
 		Addresses: []common.Address{common.HexToAddress(contractAddress)},
-		Topics:    [][]common.Hash{{crypto.Keccak256Hash([]byte(topic))}},
+		Topics: [][]common.Hash{{
+			crypto.Keccak256Hash([]byte(topics[0])),
+			crypto.Keccak256Hash([]byte(topics[1])),
+			crypto.Keccak256Hash([]byte(topics[2])),
+			crypto.Keccak256Hash([]byte(topics[3])),
+		}},
 	}
 	ticker := time.NewTicker(interval)
 	go func() {
@@ -107,17 +150,60 @@ func watchProject(ch chan<- *Project, client *ethclient.Client, instance *projec
 				slog.Error("failed to filter contract logs", "error", err)
 				continue
 			}
-			for i := range logs {
-				ev, err := instance.ParseProjectConfigUpdated(logs[i])
-				if err != nil {
-					slog.Error("failed to parse project upserted event", "error", err)
-					continue
-				}
-				ch <- &Project{
-					Uri:         ev.Uri,
-					Hash:        ev.Hash,
-					ID:          ev.ProjectId.Uint64(),
-					BlockNumber: logs[i].BlockNumber,
+			for _, evLog := range logs {
+				eventSignature := evLog.Topics[0].Hex()
+				switch eventSignature {
+				case crypto.Keccak256Hash([]byte(attributeSetTopic)).Hex():
+					ev, err := instance.ParseAttributeSet(evLog)
+					if err != nil {
+						slog.Error("failed to parse project attribute set event", "error", err)
+						continue
+					}
+					ch <- &Project{
+						ID:          ev.ProjectId.Uint64(),
+						BlockNumber: evLog.BlockNumber,
+						Attributes:  map[[32]byte][]byte{ev.Key: ev.Value},
+					}
+				case crypto.Keccak256Hash([]byte(projectPausedTopic)).Hex():
+					ev, err := instance.ParseProjectPaused(evLog)
+					if err != nil {
+						slog.Error("failed to parse project paused event", "error", err)
+						continue
+					}
+					flag := true
+					ch <- &Project{
+						ID:          ev.ProjectId.Uint64(),
+						BlockNumber: evLog.BlockNumber,
+						Paused:      &flag,
+					}
+				case crypto.Keccak256Hash([]byte(projectResumedTopic)).Hex():
+					ev, err := instance.ParseProjectResumed(evLog)
+					if err != nil {
+						slog.Error("failed to parse project resumed event", "error", err)
+						continue
+					}
+					flag := false
+					ch <- &Project{
+						ID:          ev.ProjectId.Uint64(),
+						BlockNumber: evLog.BlockNumber,
+						Paused:      &flag,
+					}
+				case crypto.Keccak256Hash([]byte(projectConfigUpdatedTopic)).Hex():
+					ev, err := instance.ParseProjectConfigUpdated(evLog)
+					if err != nil {
+						slog.Error("failed to parse project config updated event", "error", err)
+						continue
+					}
+					flag := true
+					ch <- &Project{
+						ID:          ev.ProjectId.Uint64(),
+						BlockNumber: evLog.BlockNumber,
+						Paused:      &flag,
+						Uri:         ev.Uri,
+						Hash:        ev.Hash,
+					}
+				default:
+					slog.Error("not support parse event", "event", eventSignature)
 				}
 			}
 			queriedBlockNumber = to
@@ -141,7 +227,8 @@ func ListAndWatchProver(chainEndpoint, contractAddress string) (<-chan *Prover, 
 	if err != nil {
 		return nil, errors.Wrap(err, "failed to query the latest block number")
 	}
-	watchProver(ch, client, instance, 3*time.Second, contractAddress, OperatorSetTopic, 1000, latestBlockNumber)
+	watchProver(ch, client, instance, 3*time.Second, contractAddress,
+		[]string{operatorSetTopic, nodeTypeUpdatedTopic, projectPausedTopic, proverResumedTopic}, 1000, latestBlockNumber)
 	if err := listProver(ch, instance, latestBlockNumber); err != nil {
 		return nil, err
 	}
@@ -152,23 +239,41 @@ func listProver(ch chan<- *Prover, instance *prover.Prover, targetBlockNumber ui
 	for id := uint64(1); ; id++ {
 		mp, err := instance.Operator(&bind.CallOpts{}, new(big.Int).SetUint64(id))
 		if err != nil {
-			return errors.Wrapf(err, "failed to get project meta from chain, project_id %v", id)
+			return errors.Wrapf(err, "failed to get operator from chain, prover_id %v", id)
 		}
 		if mp.String() == "" {
 			return nil
 		}
+
+		isPaused, err := instance.IsPaused(&bind.CallOpts{}, new(big.Int).SetUint64(id))
+		if err != nil {
+			return errors.Wrapf(err, "failed to get prover pause status from chain, prover_id %v", id)
+		}
+		nodeTypes, err := instance.NodeType(&bind.CallOpts{}, new(big.Int).SetUint64(id))
+		if err != nil {
+			return errors.Wrapf(err, "failed to get prover nodeTypes from chain, prover_id %v", id)
+		}
+
 		ch <- &Prover{
 			OperatorAddress: mp.String(),
 			BlockNumber:     targetBlockNumber,
+			Paused:          &isPaused,
+			NodeTypes:       nodeTypes.Uint64(),
 		}
 	}
 }
 
-func watchProver(ch chan<- *Prover, client *ethclient.Client, instance *prover.Prover, interval time.Duration, contractAddress, topic string, step, startBlockNumber uint64) {
+func watchProver(ch chan<- *Prover, client *ethclient.Client, instance *prover.Prover, interval time.Duration,
+	contractAddress string, topics []string, step, startBlockNumber uint64) {
 	queriedBlockNumber := startBlockNumber
 	query := ethereum.FilterQuery{
 		Addresses: []common.Address{common.HexToAddress(contractAddress)},
-		Topics:    [][]common.Hash{{crypto.Keccak256Hash([]byte(topic))}},
+		Topics: [][]common.Hash{{
+			crypto.Keccak256Hash([]byte(topics[0])),
+			crypto.Keccak256Hash([]byte(topics[1])),
+			crypto.Keccak256Hash([]byte(topics[2])),
+			crypto.Keccak256Hash([]byte(topics[3])),
+		}},
 	}
 	ticker := time.NewTicker(interval)
 	go func() {
@@ -194,15 +299,60 @@ func watchProver(ch chan<- *Prover, client *ethclient.Client, instance *prover.P
 				slog.Error("failed to filter contract logs", "error", err)
 				continue
 			}
-			for i := range logs {
-				ev, err := instance.ParseOperatorSet(logs[i])
-				if err != nil {
-					slog.Error("failed to parse prover upserted event", "error", err)
-					continue
-				}
-				ch <- &Prover{
-					OperatorAddress: ev.Operator.String(),
-					BlockNumber:     logs[i].BlockNumber,
+			for i, evLog := range logs {
+				eventSignature := evLog.Topics[0].Hex()
+				switch eventSignature {
+				case crypto.Keccak256Hash([]byte(operatorSetTopic)).Hex():
+					ev, err := instance.ParseOperatorSet(logs[i])
+					if err != nil {
+						slog.Error("failed to parse prover operator set event", "error", err)
+						continue
+					}
+					flag := true
+					ch <- &Prover{
+						OperatorAddress: ev.Operator.String(),
+						BlockNumber:     logs[i].BlockNumber,
+						Paused:          &flag,
+						NodeTypes:       0,
+					}
+				case crypto.Keccak256Hash([]byte(nodeTypeUpdatedTopic)).Hex():
+					ev, err := instance.ParseNodeTypeUpdated(logs[i])
+					if err != nil {
+						slog.Error("failed to parse prover nodeType updated event", "error", err)
+						continue
+					}
+					ch <- &Prover{
+						OperatorAddress: ev.Id.String(),
+						BlockNumber:     logs[i].BlockNumber,
+						NodeTypes:       ev.Typ.Uint64(),
+					}
+				case crypto.Keccak256Hash([]byte(proverPausedTopic)).Hex():
+					ev, err := instance.ParseProverPaused(logs[i])
+					if err != nil {
+						slog.Error("failed to parse prover paused event", "error", err)
+						continue
+					}
+					flag := true
+					ch <- &Prover{
+						OperatorAddress: ev.Id.String(),
+						BlockNumber:     logs[i].BlockNumber,
+						Paused:          &flag,
+					}
+				case crypto.Keccak256Hash([]byte(proverResumedTopic)).Hex():
+					ev, err := instance.ParseProverResumed(logs[i])
+					if err != nil {
+						slog.Error("failed to parse prover resumed event", "error", err)
+						continue
+					}
+					flag := false
+					ch <- &Prover{
+						OperatorAddress: ev.Id.String(),
+						BlockNumber:     logs[i].BlockNumber,
+						Paused:          &flag,
+					}
+
+				default:
+					slog.Error("not support parse event", "event", eventSignature)
 				}
 			}
 			queriedBlockNumber = to
