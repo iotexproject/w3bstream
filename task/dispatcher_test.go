388fa68b0c62047707efbea0a83bfc1ecec2e25d
huangzhiran <30522704+huangzhiran@users.noreply.github.com>
2024-04-22 14:29:25 +0800

    [task] add unit test (#438)

--- task/dispatcher_test.go
+++ task/dispatcher_test.go
@@ -3,11 +3,15 @@ package task
 import (
 	"sync"
 	"testing"
+	"time"
 
 	"github.com/agiledragon/gomonkey/v2"
+	"github.com/pkg/errors"
 	"github.com/stretchr/testify/require"
 
 	"github.com/machinefi/sprout/p2p"
+	"github.com/machinefi/sprout/persistence/contract"
+	"github.com/machinefi/sprout/project"
 	internaldispatcher "github.com/machinefi/sprout/task/internal/dispatcher"
 	"github.com/machinefi/sprout/task/internal/handler"
 	"github.com/machinefi/sprout/types"
@@ -61,7 +65,79 @@ func TestRunDispatcher(t *testing.T) {
 	p.ApplyFuncReturn(p2p.NewPubSubs, nil, nil)
 	p.ApplyFuncReturn(handler.NewTaskStateHandler, nil)
 	p.ApplyFuncReturn(dispatch, nil)
+	p.ApplyFuncReturn(dummyDispatch, nil)
 
 	err := RunDispatcher(&mockPersistence{}, nil, nil, nil, "", "", "", "", "", "", 0)
 	r.NoError(err)
+
+	err = RunDispatcher(&mockPersistence{}, nil, nil, nil, "", "", "", "", "", "/test", 0)
+	r.NoError(err)
+}
+
+func TestDummyDispatch(t *testing.T) {
+	r := require.New(t)
+
+	t.Run("FailedToGetProject", func(t *testing.T) {
+		p := gomonkey.NewPatches()
+		defer p.Reset()
+
+		pm := &project.Manager{}
+		p.ApplyMethodReturn(pm, "ProjectIDs", []uint64{1})
+		p.ApplyMethodReturn(pm, "Project", nil, errors.New(t.Name()))
+
+		err := dummyDispatch(&mockPersistence{}, nil, pm.ProjectIDs, pm.Project, &sync.Map{}, nil, nil)
+		r.ErrorContains(err, t.Name())
+	})
+	t.Run("Success", func(t *testing.T) {
+		p := gomonkey.NewPatches()
+		defer p.Reset()
+
+		pm := &project.Manager{}
+		p.ApplyMethodReturn(pm, "ProjectIDs", []uint64{1, 1})
+		p.ApplyMethodReturn(pm, "Project", &project.Project{}, nil)
+		p.ApplyMethodReturn(&p2p.PubSubs{}, "Add", nil)
+		p.ApplyFuncReturn(internaldispatcher.NewProjectDispatcher, &internaldispatcher.ProjectDispatcher{}, nil)
+
+		err := dummyDispatch(&mockPersistence{}, nil, pm.ProjectIDs, pm.Project, &sync.Map{}, &p2p.PubSubs{}, nil)
+		r.NoError(err)
+	})
+}
+
+func TestDispatch(t *testing.T) {
+	r := require.New(t)
+
+	t.Run("FailedToListAndWatchProject", func(t *testing.T) {
+		p := gomonkey.NewPatches()
+		defer p.Reset()
+
+		pm := &project.Manager{}
+		p.ApplyFuncReturn(contract.ListAndWatchProject, nil, errors.New(t.Name()))
+
+		err := dispatch(&mockPersistence{}, nil, pm.Project, &sync.Map{}, nil, nil, "", "")
+		r.ErrorContains(err, t.Name())
+	})
+	t.Run("Success", func(t *testing.T) {
+		p := gomonkey.NewPatches()
+		defer p.Reset()
+
+		ch := make(chan *contract.BlockProject, 10)
+		ch <- &contract.BlockProject{
+			Projects: map[uint64]*contract.Project{
+				1: {
+					ID: 1,
+				},
+			},
+		}
+		m := &sync.Map{}
+		pm := &project.Manager{}
+		p.ApplyFuncReturn(contract.ListAndWatchProject, ch, nil)
+		p.ApplyMethodReturn(pm, "Project", &project.Project{}, nil)
+		p.ApplyFuncReturn(internaldispatcher.NewProjectDispatcher, &internaldispatcher.ProjectDispatcher{}, nil)
+		p.ApplyMethodReturn(&p2p.PubSubs{}, "Add", nil)
+
+		err := dispatch(&mockPersistence{}, nil, pm.Project, m, &p2p.PubSubs{}, nil, "", "")
+		r.NoError(err)
+		time.Sleep(20 * time.Millisecond)
+		close(ch)
+	})
 }
