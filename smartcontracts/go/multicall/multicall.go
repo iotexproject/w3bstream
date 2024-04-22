64174201d6bfb95bcdcdcfa289414d24def6551f
huangzhiran <zhiran@iotex.me>
2024-04-22 22:51:21 +0800

    [smartcontracts] generate contract abi & go code

---
+++ smartcontracts/go/multicall/multicall.go
@@ -0,0 +1,212 @@
+// Code generated - DO NOT EDIT.
+// This file is a generated binding and any manual changes will be lost.
+
+package multicall
+
+import (
+	"errors"
+	"math/big"
+	"strings"
+
+	ethereum "github.com/ethereum/go-ethereum"
+	"github.com/ethereum/go-ethereum/accounts/abi"
+	"github.com/ethereum/go-ethereum/accounts/abi/bind"
+	"github.com/ethereum/go-ethereum/common"
+	"github.com/ethereum/go-ethereum/core/types"
+	"github.com/ethereum/go-ethereum/event"
+)
+
+// Reference imports to suppress errors if they are not otherwise used.
+var (
+	_ = errors.New
+	_ = big.NewInt
+	_ = strings.NewReader
+	_ = ethereum.NotFound
+	_ = bind.Bind
+	_ = common.Big1
+	_ = types.BloomLookup
+	_ = event.NewSubscription
+	_ = abi.ConvertType
+)
+
+// MulticallMetaData contains all meta data concerning the Multicall contract.
+var MulticallMetaData = &bind.MetaData{
+	ABI: "[{\"inputs\":[{\"internalType\":\"address[]\",\"name\":\"_targets\",\"type\":\"address[]\"},{\"internalType\":\"bytes[]\",\"name\":\"_data\",\"type\":\"bytes[]\"}],\"name\":\"multiCall\",\"outputs\":[{\"internalType\":\"bytes[]\",\"name\":\"results_\",\"type\":\"bytes[]\"}],\"stateMutability\":\"view\",\"type\":\"function\"}]",
+}
+
+// MulticallABI is the input ABI used to generate the binding from.
+// Deprecated: Use MulticallMetaData.ABI instead.
+var MulticallABI = MulticallMetaData.ABI
+
+// Multicall is an auto generated Go binding around an Ethereum contract.
+type Multicall struct {
+	MulticallCaller     // Read-only binding to the contract
+	MulticallTransactor // Write-only binding to the contract
+	MulticallFilterer   // Log filterer for contract events
+}
+
+// MulticallCaller is an auto generated read-only Go binding around an Ethereum contract.
+type MulticallCaller struct {
+	contract *bind.BoundContract // Generic contract wrapper for the low level calls
+}
+
+// MulticallTransactor is an auto generated write-only Go binding around an Ethereum contract.
+type MulticallTransactor struct {
+	contract *bind.BoundContract // Generic contract wrapper for the low level calls
+}
+
+// MulticallFilterer is an auto generated log filtering Go binding around an Ethereum contract events.
+type MulticallFilterer struct {
+	contract *bind.BoundContract // Generic contract wrapper for the low level calls
+}
+
+// MulticallSession is an auto generated Go binding around an Ethereum contract,
+// with pre-set call and transact options.
+type MulticallSession struct {
+	Contract     *Multicall        // Generic contract binding to set the session for
+	CallOpts     bind.CallOpts     // Call options to use throughout this session
+	TransactOpts bind.TransactOpts // Transaction auth options to use throughout this session
+}
+
+// MulticallCallerSession is an auto generated read-only Go binding around an Ethereum contract,
+// with pre-set call options.
+type MulticallCallerSession struct {
+	Contract *MulticallCaller // Generic contract caller binding to set the session for
+	CallOpts bind.CallOpts    // Call options to use throughout this session
+}
+
+// MulticallTransactorSession is an auto generated write-only Go binding around an Ethereum contract,
+// with pre-set transact options.
+type MulticallTransactorSession struct {
+	Contract     *MulticallTransactor // Generic contract transactor binding to set the session for
+	TransactOpts bind.TransactOpts    // Transaction auth options to use throughout this session
+}
+
+// MulticallRaw is an auto generated low-level Go binding around an Ethereum contract.
+type MulticallRaw struct {
+	Contract *Multicall // Generic contract binding to access the raw methods on
+}
+
+// MulticallCallerRaw is an auto generated low-level read-only Go binding around an Ethereum contract.
+type MulticallCallerRaw struct {
+	Contract *MulticallCaller // Generic read-only contract binding to access the raw methods on
+}
+
+// MulticallTransactorRaw is an auto generated low-level write-only Go binding around an Ethereum contract.
+type MulticallTransactorRaw struct {
+	Contract *MulticallTransactor // Generic write-only contract binding to access the raw methods on
+}
+
+// NewMulticall creates a new instance of Multicall, bound to a specific deployed contract.
+func NewMulticall(address common.Address, backend bind.ContractBackend) (*Multicall, error) {
+	contract, err := bindMulticall(address, backend, backend, backend)
+	if err != nil {
+		return nil, err
+	}
+	return &Multicall{MulticallCaller: MulticallCaller{contract: contract}, MulticallTransactor: MulticallTransactor{contract: contract}, MulticallFilterer: MulticallFilterer{contract: contract}}, nil
+}
+
+// NewMulticallCaller creates a new read-only instance of Multicall, bound to a specific deployed contract.
+func NewMulticallCaller(address common.Address, caller bind.ContractCaller) (*MulticallCaller, error) {
+	contract, err := bindMulticall(address, caller, nil, nil)
+	if err != nil {
+		return nil, err
+	}
+	return &MulticallCaller{contract: contract}, nil
+}
+
+// NewMulticallTransactor creates a new write-only instance of Multicall, bound to a specific deployed contract.
+func NewMulticallTransactor(address common.Address, transactor bind.ContractTransactor) (*MulticallTransactor, error) {
+	contract, err := bindMulticall(address, nil, transactor, nil)
+	if err != nil {
+		return nil, err
+	}
+	return &MulticallTransactor{contract: contract}, nil
+}
+
+// NewMulticallFilterer creates a new log filterer instance of Multicall, bound to a specific deployed contract.
+func NewMulticallFilterer(address common.Address, filterer bind.ContractFilterer) (*MulticallFilterer, error) {
+	contract, err := bindMulticall(address, nil, nil, filterer)
+	if err != nil {
+		return nil, err
+	}
+	return &MulticallFilterer{contract: contract}, nil
+}
+
+// bindMulticall binds a generic wrapper to an already deployed contract.
+func bindMulticall(address common.Address, caller bind.ContractCaller, transactor bind.ContractTransactor, filterer bind.ContractFilterer) (*bind.BoundContract, error) {
+	parsed, err := MulticallMetaData.GetAbi()
+	if err != nil {
+		return nil, err
+	}
+	return bind.NewBoundContract(address, *parsed, caller, transactor, filterer), nil
+}
+
+// Call invokes the (constant) contract method with params as input values and
+// sets the output to result. The result type might be a single field for simple
+// returns, a slice of interfaces for anonymous returns and a struct for named
+// returns.
+func (_Multicall *MulticallRaw) Call(opts *bind.CallOpts, result *[]interface{}, method string, params ...interface{}) error {
+	return _Multicall.Contract.MulticallCaller.contract.Call(opts, result, method, params...)
+}
+
+// Transfer initiates a plain transaction to move funds to the contract, calling
+// its default method if one is available.
+func (_Multicall *MulticallRaw) Transfer(opts *bind.TransactOpts) (*types.Transaction, error) {
+	return _Multicall.Contract.MulticallTransactor.contract.Transfer(opts)
+}
+
+// Transact invokes the (paid) contract method with params as input values.
+func (_Multicall *MulticallRaw) Transact(opts *bind.TransactOpts, method string, params ...interface{}) (*types.Transaction, error) {
+	return _Multicall.Contract.MulticallTransactor.contract.Transact(opts, method, params...)
+}
+
+// Call invokes the (constant) contract method with params as input values and
+// sets the output to result. The result type might be a single field for simple
+// returns, a slice of interfaces for anonymous returns and a struct for named
+// returns.
+func (_Multicall *MulticallCallerRaw) Call(opts *bind.CallOpts, result *[]interface{}, method string, params ...interface{}) error {
+	return _Multicall.Contract.contract.Call(opts, result, method, params...)
+}
+
+// Transfer initiates a plain transaction to move funds to the contract, calling
+// its default method if one is available.
+func (_Multicall *MulticallTransactorRaw) Transfer(opts *bind.TransactOpts) (*types.Transaction, error) {
+	return _Multicall.Contract.contract.Transfer(opts)
+}
+
+// Transact invokes the (paid) contract method with params as input values.
+func (_Multicall *MulticallTransactorRaw) Transact(opts *bind.TransactOpts, method string, params ...interface{}) (*types.Transaction, error) {
+	return _Multicall.Contract.contract.Transact(opts, method, params...)
+}
+
+// MultiCall is a free data retrieval call binding the contract method 0xda5b4ffd.
+//
+// Solidity: function multiCall(address[] _targets, bytes[] _data) view returns(bytes[] results_)
+func (_Multicall *MulticallCaller) MultiCall(opts *bind.CallOpts, _targets []common.Address, _data [][]byte) ([][]byte, error) {
+	var out []interface{}
+	err := _Multicall.contract.Call(opts, &out, "multiCall", _targets, _data)
+
+	if err != nil {
+		return *new([][]byte), err
+	}
+
+	out0 := *abi.ConvertType(out[0], new([][]byte)).(*[][]byte)
+
+	return out0, err
+
+}
+
+// MultiCall is a free data retrieval call binding the contract method 0xda5b4ffd.
+//
+// Solidity: function multiCall(address[] _targets, bytes[] _data) view returns(bytes[] results_)
+func (_Multicall *MulticallSession) MultiCall(_targets []common.Address, _data [][]byte) ([][]byte, error) {
+	return _Multicall.Contract.MultiCall(&_Multicall.CallOpts, _targets, _data)
+}
+
+// MultiCall is a free data retrieval call binding the contract method 0xda5b4ffd.
+//
+// Solidity: function multiCall(address[] _targets, bytes[] _data) view returns(bytes[] results_)
+func (_Multicall *MulticallCallerSession) MultiCall(_targets []common.Address, _data [][]byte) ([][]byte, error) {
+	return _Multicall.Contract.MultiCall(&_Multicall.CallOpts, _targets, _data)
+}
