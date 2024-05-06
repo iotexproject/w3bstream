f6ae19819ae5f6ed1404b954ab8db91129e112cd
huangzhiran <30522704+huangzhiran@users.noreply.github.com>
2024-05-06 11:22:45 +0800

    [task] split task pkg to dispatcher&processor pkg (#476)

--- cmd/coordinator/main.go
+++ cmd/coordinator/main.go
@@ -17,7 +17,7 @@ import (
 	"github.com/machinefi/sprout/persistence/contract"
 	"github.com/machinefi/sprout/persistence/postgres"
 	"github.com/machinefi/sprout/project"
-	"github.com/machinefi/sprout/task"
+	"github.com/machinefi/sprout/task/dispatcher"
 )
 
 func main() {
@@ -66,12 +66,12 @@ func main() {
 	}
 
 	if local {
-		err = task.RunLocalDispatcher(persistence, datasource.NewPostgres, projectManager.ProjectIDs, projectManager.Project,
+		err = dispatcher.RunLocalDispatcher(persistence, datasource.NewPostgres, projectManager,
 			conf.OperatorPrivateKey, conf.OperatorPrivateKeyED25519, conf.BootNodeMultiAddr, sequencerPubKey, conf.IoTeXChainID)
 	} else {
-		err = task.RunDispatcher(persistence, datasource.NewPostgres, projectManager.Project, conf.BootNodeMultiAddr,
+		err = dispatcher.RunDispatcher(persistence, datasource.NewPostgres, projectManager, conf.BootNodeMultiAddr,
 			conf.OperatorPrivateKey, conf.OperatorPrivateKeyED25519, sequencerPubKey, conf.IoTeXChainID,
-			dispatcherNotification, contractPersistence.LatestProjects, contractPersistence.LatestProvers)
+			dispatcherNotification, contractPersistence)
 	}
 	if err != nil {
 		log.Fatal(errors.Wrap(err, "failed to run dispatcher"))
