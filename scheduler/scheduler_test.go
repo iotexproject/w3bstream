676696a4dd361260dc6ebcce548d369895a45d63
huangzhiran <30522704+huangzhiran@users.noreply.github.com>
2024-05-08 21:28:39 +0800

    [scheduler] add unit test (#488)

--- scheduler/scheduler_test.go
+++ scheduler/scheduler_test.go
@@ -1,85 +1,269 @@
 package scheduler
 
 import (
+	"sync"
+	"sync/atomic"
 	"testing"
 
 	"github.com/agiledragon/gomonkey/v2"
+	"github.com/ethereum/go-ethereum/common"
+	"github.com/pkg/errors"
+	"github.com/stretchr/testify/require"
 
 	"github.com/machinefi/sprout/p2p"
+	"github.com/machinefi/sprout/persistence/contract"
 	"github.com/machinefi/sprout/project"
+	"github.com/machinefi/sprout/util/distance"
 )
 
-// func TestScheduler_schedule(t *testing.T) {
-// 	r := require.New(t)
-// 	p := gomonkey.NewPatches()
-// 	defer p.Reset()
+func TestScheduler_schedule(t *testing.T) {
+	r := require.New(t)
+	scheduledProverID := atomic.Uint64{}
+	handleProjectProvers := func(projectID uint64, proverIDs []uint64) {
+		scheduledProverID.Store(proverIDs[0])
+	}
+	t.Run("NoProject", func(t *testing.T) {
+		chainHead := make(chan uint64, 10)
+		chainHead <- 1
+		close(chainHead)
+		s := &scheduler{
+			chainHead:            chainHead,
+			projectOffsets:       &sync.Map{},
+			epoch:                1,
+			handleProjectProvers: handleProjectProvers,
+		}
+		s.schedule()
+		r.Equal(uint64(0), scheduledProverID.Load())
+	})
+	t.Run("ProjectAlreadyScheduled", func(t *testing.T) {
+		chainHead := make(chan uint64, 10)
+		chainHead <- 1
+		close(chainHead)
+		s := &scheduler{
+			chainHead:            chainHead,
+			projectOffsets:       &sync.Map{},
+			epoch:                1,
+			handleProjectProvers: handleProjectProvers,
+		}
+		p := &projectOffset{}
+		p.scheduledBlockNumber.Store(1)
+		s.projectOffsets.Store(uint64(0), p)
+		s.schedule()
+		r.Equal(uint64(0), scheduledProverID.Load())
+	})
+	t.Run("ContractProjectNotExist", func(t *testing.T) {
+		p := gomonkey.NewPatches()
+		defer p.Reset()
 
-// 	p.ApplyMethodReturn(&p2p.PubSubs{}, "Add", nil)
+		paused := false
+		pm := &contract.Contract{}
+		p.ApplyMethodReturn(pm, "Provers", []*contract.Prover{{ID: 1, Paused: &paused}})
+		p.ApplyMethodReturn(pm, "Project", nil)
 
-// 	scheduledProverID := atomic.Uint64{}
+		chainHead := make(chan uint64, 10)
+		chainHead <- 1
+		close(chainHead)
+		s := &scheduler{
+			contractProvers:      pm.Provers,
+			contractProject:      pm.Project,
+			chainHead:            chainHead,
+			projectOffsets:       &sync.Map{},
+			epoch:                1,
+			handleProjectProvers: handleProjectProvers,
+		}
+		po := &projectOffset{}
+		po.projectIDs.Store(uint64(1), true)
+		s.projectOffsets.Store(uint64(0), po)
+		s.schedule()
+		r.Equal(uint64(0), scheduledProverID.Load())
+	})
+	t.Run("FailedToParseProjectRequiredProverAmount", func(t *testing.T) {
+		p := gomonkey.NewPatches()
+		defer p.Reset()
 
-// 	s := &scheduler{
-// 		contractProver: &contractProver{
-// 			epoch:  1,
-// 			blocks: list.New(),
-// 		},
-// 		contractProject: &contractProject{
-// 			epoch:  10,
-// 			blocks: list.New(),
-// 		},
-// 		projectOffsets: &sync.Map{},
-// 		epoch:          1,
-// 		chainHead:      make(chan uint64, 10),
-// 		proverID:       1,
-// 		handleProjectProvers: func(projectID uint64, proverIDs []uint64) {
-// 			scheduledProverID.Store(proverIDs[0])
-// 		},
-// 	}
+		paused := false
+		pm := &contract.Contract{}
+		p.ApplyMethodReturn(pm, "Provers", []*contract.Prover{{ID: 1, Paused: &paused}})
+		p.ApplyMethodReturn(pm, "Project", &contract.Project{
+			Attributes: map[common.Hash][]byte{contract.RequiredProverAmountHash: []byte("err")},
+		})
 
-// 	s.chainHead <- 100
-// 	s.contractProver.add(&contract.BlockProver{
-// 		BlockNumber: 100,
-// 		Provers: map[uint64]*contract.Prover{
-// 			1: {
-// 				ID: 1,
-// 			},
-// 		},
-// 	})
-// 	pf := &projectOffset{}
-// 	projectID := uint64(1)
-// 	pf.projectIDs.Store(projectID, true)
-// 	s.projectOffsets.Store(hash.Keccak256Uint64(projectID).Big().Uint64()%s.epoch, pf)
-// 	go s.schedule()
-// 	for scheduledProverID.Load() == 0 {
-// 	}
-// 	close(s.chainHead)
-// 	r.Equal(scheduledProverID.Load(), uint64(1))
-// }
+		chainHead := make(chan uint64, 10)
+		chainHead <- 1
+		close(chainHead)
+		s := &scheduler{
+			contractProvers:      pm.Provers,
+			contractProject:      pm.Project,
+			chainHead:            chainHead,
+			projectOffsets:       &sync.Map{},
+			epoch:                1,
+			handleProjectProvers: handleProjectProvers,
+		}
+		po := &projectOffset{}
+		po.projectIDs.Store(uint64(1), true)
+		s.projectOffsets.Store(uint64(0), po)
+		s.schedule()
+		r.Equal(uint64(0), scheduledProverID.Load())
+	})
+	t.Run("NoEnoughResource", func(t *testing.T) {
+		p := gomonkey.NewPatches()
+		defer p.Reset()
 
-// func TestRun(t *testing.T) {
-// 	r := require.New(t)
-// 	p := gomonkey.NewPatches()
-// 	defer p.Reset()
+		paused := false
+		pm := &contract.Contract{}
+		p.ApplyMethodReturn(pm, "Provers", []*contract.Prover{{ID: 1, Paused: &paused}})
+		p.ApplyMethodReturn(pm, "Project", &contract.Project{
+			Attributes: map[common.Hash][]byte{contract.RequiredProverAmountHash: []byte("10")},
+		})
 
-// 	p.ApplyFuncReturn(contract.ListAndWatchProver, make(chan *contract.BlockProver), nil)
-// 	p.ApplyFuncReturn(contract.ListAndWatchProject, make(chan *contract.BlockProject), nil)
-// 	p.ApplyFuncReturn(watchChainHead, nil)
+		chainHead := make(chan uint64, 10)
+		chainHead <- 1
+		close(chainHead)
+		s := &scheduler{
+			contractProvers:      pm.Provers,
+			contractProject:      pm.Project,
+			chainHead:            chainHead,
+			projectOffsets:       &sync.Map{},
+			epoch:                1,
+			handleProjectProvers: handleProjectProvers,
+		}
+		po := &projectOffset{}
+		po.projectIDs.Store(uint64(1), true)
+		s.projectOffsets.Store(uint64(0), po)
+		s.schedule()
+		r.Equal(uint64(0), scheduledProverID.Load())
+	})
+	t.Run("TheProjectNotScheduledToThisProver", func(t *testing.T) {
+		p := gomonkey.NewPatches()
+		defer p.Reset()
 
-// 	err := Run(1, "", "", "", "", 1, nil, nil, nil)
-// 	r.NoError(err)
-// }
+		paused := false
+		pm := &contract.Contract{}
+		p.ApplyMethodReturn(pm, "Provers", []*contract.Prover{{ID: 1, Paused: &paused}})
+		p.ApplyMethodReturn(pm, "Project", &contract.Project{ID: 1})
+		p.ApplyFuncReturn(distance.Sort, []uint64{100})
+		p.ApplyMethodReturn(&p2p.PubSubs{}, "Delete")
 
-func TestDummySchedule(t *testing.T) {
+		chainHead := make(chan uint64, 10)
+		chainHead <- 1
+		close(chainHead)
+		s := &scheduler{
+			contractProvers:      pm.Provers,
+			contractProject:      pm.Project,
+			chainHead:            chainHead,
+			projectOffsets:       &sync.Map{},
+			epoch:                1,
+			handleProjectProvers: handleProjectProvers,
+		}
+		po := &projectOffset{}
+		po.projectIDs.Store(uint64(1), true)
+		s.projectOffsets.Store(uint64(0), po)
+		s.schedule()
+		r.Equal(uint64(0), scheduledProverID.Load())
+	})
+	t.Run("FailedToAddPubSubs", func(t *testing.T) {
+		p := gomonkey.NewPatches()
+		defer p.Reset()
+
+		paused := false
+		pm := &contract.Contract{}
+		p.ApplyMethodReturn(pm, "Provers", []*contract.Prover{{ID: 1, Paused: &paused}})
+		p.ApplyMethodReturn(pm, "Project", &contract.Project{ID: 1})
+		p.ApplyMethodReturn(&p2p.PubSubs{}, "Add", errors.New(t.Name()))
+
+		chainHead := make(chan uint64, 10)
+		chainHead <- 1
+		close(chainHead)
+		s := &scheduler{
+			proverID:             1,
+			contractProvers:      pm.Provers,
+			contractProject:      pm.Project,
+			chainHead:            chainHead,
+			projectOffsets:       &sync.Map{},
+			epoch:                1,
+			handleProjectProvers: handleProjectProvers,
+		}
+		po := &projectOffset{}
+		po.projectIDs.Store(uint64(1), true)
+		s.projectOffsets.Store(uint64(0), po)
+		s.schedule()
+		r.Equal(uint64(1), scheduledProverID.Load())
+	})
+	t.Run("Success", func(t *testing.T) {
+		p := gomonkey.NewPatches()
+		defer p.Reset()
+
+		paused := false
+		pm := &contract.Contract{}
+		p.ApplyMethodReturn(pm, "Provers", []*contract.Prover{{ID: 1, Paused: &paused}})
+		p.ApplyMethodReturn(pm, "Project", &contract.Project{ID: 1})
+		p.ApplyMethodReturn(&p2p.PubSubs{}, "Add", nil)
+
+		chainHead := make(chan uint64, 10)
+		chainHead <- 1
+		close(chainHead)
+		s := &scheduler{
+			proverID:             1,
+			contractProvers:      pm.Provers,
+			contractProject:      pm.Project,
+			chainHead:            chainHead,
+			projectOffsets:       &sync.Map{},
+			epoch:                1,
+			handleProjectProvers: handleProjectProvers,
+		}
+		po := &projectOffset{}
+		po.projectIDs.Store(uint64(1), true)
+		s.projectOffsets.Store(uint64(0), po)
+		s.schedule()
+		r.Equal(uint64(1), scheduledProverID.Load())
+		r.Equal(po.scheduledBlockNumber.Load(), uint64(1))
+	})
+}
+
+func TestRun(t *testing.T) {
+	r := require.New(t)
 	p := gomonkey.NewPatches()
 	defer p.Reset()
 
-	ps := &p2p.PubSubs{}
-	f := func(uint64, []uint64) {
-	}
-	pm := &project.Manager{}
+	pm := &contract.Contract{}
+	p.ApplyMethodReturn(pm, "LatestProjects", []*contract.Project{{ID: 1}})
+	p.ApplyPrivateMethod(&scheduler{}, "schedule", func() {})
 
-	p.ApplyMethodReturn(pm, "ProjectIDs", []uint64{1})
-	p.ApplyMethodReturn(&p2p.PubSubs{}, "Add", nil)
+	notification := make(chan *contract.Project, 10)
+	notification <- &contract.Project{ID: 2}
+	close(notification)
 
-	RunLocal(ps, f, pm.ProjectIDs)
+	err := Run(10, 1, nil, nil, nil, notification, nil, pm.LatestProjects, nil)
+	r.NoError(err)
+}
+
+func TestRunLocal(t *testing.T) {
+	t.Run("FailedToAddPubsubs", func(t *testing.T) {
+		p := gomonkey.NewPatches()
+		defer p.Reset()
+
+		ps := &p2p.PubSubs{}
+		f := func(uint64, []uint64) {
+		}
+		pm := &project.Manager{}
+
+		p.ApplyMethodReturn(pm, "ProjectIDs", []uint64{1})
+		p.ApplyMethodReturn(&p2p.PubSubs{}, "Add", errors.New(t.Name()))
+
+		RunLocal(ps, f, pm.ProjectIDs)
+	})
+	t.Run("Success", func(t *testing.T) {
+		p := gomonkey.NewPatches()
+		defer p.Reset()
+
+		ps := &p2p.PubSubs{}
+		f := func(uint64, []uint64) {
+		}
+		pm := &project.Manager{}
+
+		p.ApplyMethodReturn(pm, "ProjectIDs", []uint64{1})
+		p.ApplyMethodReturn(&p2p.PubSubs{}, "Add", nil)
+
+		RunLocal(ps, f, pm.ProjectIDs)
+	})
 }
