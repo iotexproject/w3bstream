2e5afce7cbf2ad7357c620bad74128f6395028c1
huangzhiran <30522704+huangzhiran@users.noreply.github.com>
2024-05-10 09:20:08 +0800

    [task/dispatcher] add window unit test (#497)

---
+++ task/dispatcher/window_test.go
@@ -0,0 +1,43 @@
+package dispatcher
+
+import (
+	"testing"
+
+	"github.com/agiledragon/gomonkey/v2"
+	"github.com/machinefi/sprout/task"
+	"github.com/pkg/errors"
+	"github.com/stretchr/testify/require"
+)
+
+func TestWindow(t *testing.T) {
+	r := require.New(t)
+	p := gomonkey.NewPatches()
+	defer p.Reset()
+
+	tk := &task.Task{ID: 1}
+	dt := &dispatchedTask{
+		task: tk,
+	}
+	dt.finished.Store(true)
+	ps := &mockPersistence{}
+	p.ApplyPrivateMethod(dt, "handleState", func(s *task.StateLog) {})
+	p.ApplyFuncReturn(newDispatchedTask, dt)
+	p.ApplyMethodReturn(ps, "UpsertProcessedTask", errors.New(t.Name()))
+	w := newWindow(10, nil, nil, ps)
+	r.True(w.isEmpty())
+	r.False(w.isFull())
+
+	w.produce(tk)
+	r.False(w.isEmpty())
+
+	w.consume(&task.StateLog{TaskID: 1})
+	r.True(w.isEmpty())
+
+	dt.finished.Store(false)
+
+	w.produce(tk)
+	r.False(w.isEmpty())
+
+	w.consume(&task.StateLog{TaskID: 1})
+	r.False(w.isEmpty())
+}
