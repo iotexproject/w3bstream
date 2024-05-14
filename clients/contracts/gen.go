5c815356e26c56d5b243f4849057e05cb0699b93
sincos.ubuntu <birdyfj@gmail.com>
2024-05-14 15:30:46 +0800

    chore: add ioID-contracts abis and generate golang code

--- clients/contracts/gen.go
+++ clients/contracts/gen.go
@@ -1,3 +1,4 @@
 package contracts
 
 //go:generate abigen --abi abis/ioIDRegistry.json --pkg contracts --type IoIDRegistry -out ./ioIDRegistry.go
+//go:generate abigen --abi abis/ioID.json --pkg contracts --type IoID -out ./ioID.go
