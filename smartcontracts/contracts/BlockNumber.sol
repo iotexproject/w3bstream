77948f78e4240995f2e183d7f39b5edc42d7f851
huangzhiran <zhiran@iotex.me>
2024-06-04 17:14:46 +0800

    [contract] delete unuse contract code

--- smartcontracts/contracts/BlockNumber.sol
+++
@@ -1,8 +0,0 @@
-// SPDX-License-Identifier: MIT
-pragma solidity ^0.8.19;
-
-contract BlockNumber {
-    function blockNumber() external view returns (uint256) {
-        return block.number;
-    }
-}
