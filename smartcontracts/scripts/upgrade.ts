a1506d031c6886ffd6f47fa76c02d372018c096f
ququzone <xueping.yang@gmail.com>
2024-07-03 17:08:53 +0800

    upgrade router contract

--- smartcontracts/scripts/upgrade.ts
+++ smartcontracts/scripts/upgrade.ts
@@ -6,6 +6,12 @@ async function main() {
     await upgrades.upgradeProxy(process.env.W3BSTREAM_PROJECT, W3bstreamProject, {});
     console.log(`Upgrade W3bstreamProject ${process.env.W3BSTREAM_PROJECT} successfull!`);
   }
+
+  if (process.env.W3BSTREAM_ROUTER) {
+    const W3bstreamRouter = await ethers.getContractFactory('W3bstreamRouter');
+    await upgrades.upgradeProxy(process.env.W3BSTREAM_ROUTER, W3bstreamRouter, {});
+    console.log(`Upgrade W3bstreamRouter ${process.env.W3BSTREAM_ROUTER} successfull!`);
+  }
 }
 
 main().catch(err => {
