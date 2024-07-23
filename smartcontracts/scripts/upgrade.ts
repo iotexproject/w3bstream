80fb090206b7ed1aeb74e86fff3da4b13460112d
hunshenshi <289517357@qq.com>
2024-07-23 17:30:42 +0800

    feat(contract): deploy vmtype contract and update prover contract

--- smartcontracts/scripts/upgrade.ts
+++ smartcontracts/scripts/upgrade.ts
@@ -12,6 +12,12 @@ async function main() {
     await upgrades.upgradeProxy(process.env.W3BSTREAM_ROUTER, W3bstreamRouter, {});
     console.log(`Upgrade W3bstreamRouter ${process.env.W3BSTREAM_ROUTER} successfull!`);
   }
+
+  if (process.env.W3BSTREAM_PROVER) {
+    const W3bstreamProver = await ethers.getContractFactory('W3bstreamProver');
+    await upgrades.upgradeProxy(process.env.W3BSTREAM_PROVER, W3bstreamProver, {});
+    console.log(`Upgrade W3bstreamRouter ${process.env.W3BSTREAM_PROVER} successfull!`);
+  }
 }
 
 main().catch(err => {
