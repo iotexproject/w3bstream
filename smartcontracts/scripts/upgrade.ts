2e6a0a2b6918bbf46ffc6debb6d92f3374c2c250
hunshenshi <289517357@qq.com>
2024-07-24 16:04:45 +0800

    feat(contract): update vmtype contract

--- smartcontracts/scripts/upgrade.ts
+++ smartcontracts/scripts/upgrade.ts
@@ -3,20 +3,30 @@ import { ethers, upgrades } from 'hardhat';
 async function main() {
   if (process.env.W3BSTREAM_PROJECT) {
     const W3bstreamProject = await ethers.getContractFactory('W3bstreamProject');
+    await upgrades.forceImport(process.env.W3BSTREAM_PROJECT, W3bstreamProject);
     await upgrades.upgradeProxy(process.env.W3BSTREAM_PROJECT, W3bstreamProject, {});
     console.log(`Upgrade W3bstreamProject ${process.env.W3BSTREAM_PROJECT} successfull!`);
   }
 
   if (process.env.W3BSTREAM_ROUTER) {
     const W3bstreamRouter = await ethers.getContractFactory('W3bstreamRouter');
+    await upgrades.forceImport(process.env.W3BSTREAM_ROUTER, W3bstreamRouter);
     await upgrades.upgradeProxy(process.env.W3BSTREAM_ROUTER, W3bstreamRouter, {});
     console.log(`Upgrade W3bstreamRouter ${process.env.W3BSTREAM_ROUTER} successfull!`);
   }
 
   if (process.env.W3BSTREAM_PROVER) {
     const W3bstreamProver = await ethers.getContractFactory('W3bstreamProver');
+    await upgrades.forceImport(process.env.W3BSTREAM_PROVER, W3bstreamProver);
     await upgrades.upgradeProxy(process.env.W3BSTREAM_PROVER, W3bstreamProver, {});
     console.log(`Upgrade W3bstreamRouter ${process.env.W3BSTREAM_PROVER} successfull!`);
+  }
+
+  if (process.env.W3BSTREAM_VMTYPE) {
+    const W3bstreamVMType = await ethers.getContractFactory('W3bstreamVMType');
+    await upgrades.forceImport(process.env.W3BSTREAM_VMTYPE, W3bstreamVMType);
+    await upgrades.upgradeProxy(process.env.W3BSTREAM_VMTYPE, W3bstreamVMType, {});
+    console.log(`Upgrade W3bstreamVMType ${process.env.W3BSTREAM_VMTYPE} successfull!`);
   }
 }
 
