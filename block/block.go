a4559f169a484df9224525280d4605e62dce2f84
huangzhiran <zhiran@iotex.me>
2024-09-11 21:01:54 +0800

    add JSON-RPC mock impl

--- block/block.go
+++ block/block.go
@@ -1,6 +1,11 @@
 package block
 
 import (
+	"crypto/sha256"
+	"encoding/binary"
+	"math/big"
+
+	"github.com/btcsuite/btcd/blockchain"
 	"github.com/ethereum/go-ethereum/common"
 	"github.com/ethereum/go-ethereum/crypto"
 
@@ -11,7 +16,7 @@ type Header struct {
 	Meta       [4]byte
 	PrevHash   common.Hash
 	MerkleRoot [32]byte
-	Difficulty [4]byte
+	Difficulty uint32
 	Nonce      [8]byte
 }
 
@@ -21,5 +26,28 @@ type Block struct {
 }
 
 func (h *Header) Hash() common.Hash {
-	return crypto.Keccak256Hash(h.Meta[:], h.PrevHash[:], h.MerkleRoot[:], h.Difficulty[:], h.Nonce[:])
+	numberBytes := make([]byte, 4)
+	binary.BigEndian.PutUint32(numberBytes, h.Difficulty)
+	return crypto.Keccak256Hash(h.Meta[:], h.PrevHash[:], h.MerkleRoot[:], numberBytes[:], h.Nonce[:])
+}
+
+func (h *Header) Sha256Hash() common.Hash {
+	numberBytes := make([]byte, 4)
+	binary.BigEndian.PutUint32(numberBytes, h.Difficulty)
+	data := []byte{}
+	data = append(data, h.Meta[:]...)
+	data = append(data, h.PrevHash[:]...)
+	data = append(data, h.MerkleRoot[:]...)
+	data = append(data, numberBytes[:]...)
+	data = append(data, h.Nonce[:]...)
+	return sha256.Sum256(data)
+}
+
+func (h *Header) IsValid() bool {
+	target := blockchain.CompactToBig(h.Difficulty)
+	firstHash := h.Sha256Hash()
+	finalHash := sha256.Sum256(firstHash[:])
+	hashInt := new(big.Int).SetBytes(finalHash[:])
+
+	return hashInt.Cmp(target) == -1
 }
---
+++ cmd/sequencer/api/types.go
@@ -0,0 +1,71 @@
+package api
+
+import (
+	"encoding/hex"
+
+	"github.com/ethereum/go-ethereum/common"
+	"github.com/iotexproject/w3bstream/block"
+)
+
+type jsonRpcReq struct {
+	ID      uint64 `json:"id"             binding:"required"`
+	Version string `json:"jsonrpc"        binding:"required"`
+	Method  string `json:"method"         binding:"required"`
+	Params  []byte `json:"params"`
+}
+
+type jsonRpcRsp struct {
+	ID      uint64 `json:"id"`
+	Version string `json:"jsonrpc"`
+	Error   string `json:"error"`
+	Result  any    `json:"result"`
+}
+
+type blockTemplate struct {
+	Meta          string `json:"meta"`
+	PrevBlockHash string `json:"previousblockhash"`
+	MerkleRoot    string `json:"merkleroot"`
+	Difficulty    uint32 `json:"difficulty"`
+	Ts            uint64 `json:"ts"`
+	NonceRange    string `json:"noncerange"`
+}
+
+type submitBlockParam struct {
+	Meta          string `json:"meta"`
+	PrevBlockHash string `json:"previousblockhash"`
+	MerkleRoot    string `json:"merkleroot"`
+	Difficulty    uint32 `json:"difficulty"`
+	Ts            uint64 `json:"ts"`
+	Nonce         string `json:"nonce"`
+}
+
+func (p *submitBlockParam) toBlockHeader() (*block.Header, error) {
+	meta, err := hex.DecodeString(p.Meta)
+	if err != nil {
+		return nil, err
+	}
+	prevBlockHash, err := hex.DecodeString(p.PrevBlockHash)
+	if err != nil {
+		return nil, err
+	}
+	merkleRoot, err := hex.DecodeString(p.MerkleRoot)
+	if err != nil {
+		return nil, err
+	}
+	nonce, err := hex.DecodeString(p.Nonce)
+	if err != nil {
+		return nil, err
+	}
+	h := &block.Header{
+		Meta:       [4]byte{},
+		PrevHash:   common.Hash{},
+		MerkleRoot: [32]byte{},
+		Nonce:      [8]byte{},
+	}
+	copy(h.Meta[:], meta)
+	copy(h.PrevHash[:], prevBlockHash)
+	copy(h.MerkleRoot[:], merkleRoot)
+	copy(h.Nonce[:], nonce)
+	h.Difficulty = p.Difficulty
+	return h, nil
+}
