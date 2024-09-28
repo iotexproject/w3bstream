2b61e5c08f7d795a7a24e3641da13a7753c48a9e
huangzhiran <zhiran@iotex.me>
2024-09-28 21:43:33 +0800

    impl task processor

--- cmd/prover/db/db.go
+++ cmd/prover/db/db.go
@@ -1,6 +1,8 @@
 package db
 
 import (
+	"bytes"
+
 	"github.com/ethereum/go-ethereum/common"
 	"github.com/pkg/errors"
 	"gorm.io/gorm"
@@ -28,12 +30,14 @@ type projectFile struct {
 
 type task struct {
 	gorm.Model
-	TaskID    common.Hash `gorm:"index:task_fetch,not null"`
-	ProjectID uint64      `gorm:"index:task_fetch,not null"`
+	TaskID    common.Hash `gorm:"uniqueIndex:task_uniq,not null"`
+	ProjectID uint64      `gorm:"uniqueIndex:task_uniq,not null"`
+	Processed bool        `gorm:"index:unprocessed_task,not null,default:false"`
 }
 
 type DB struct {
-	db *gorm.DB
+	db     *gorm.DB
+	prover common.Address
 }
 
 func (p *DB) ScannedBlockNumber() (uint64, error) {
@@ -106,13 +110,25 @@ func (p *DB) UpsertProjectFile(projectID uint64, file []byte, hash common.Hash) 
 	return errors.Wrap(err, "failed to upsert project file")
 }
 
-func (p *DB) CreateTask(projectID uint64, taskID common.Hash) error {
+func (p *DB) CreateTask(projectID uint64, taskID common.Hash, prover common.Address) error {
+	if !bytes.Equal(prover[:], p.prover[:]) {
+		return nil
+	}
 	t := &task{
 		TaskID:    taskID,
 		ProjectID: projectID,
+		Processed: false,
 	}
 	err := p.db.Create(t).Error
 	return errors.Wrap(err, "failed to create task")
+}
+
+func (p *DB) ProcessTask(projectID uint64, taskID common.Hash) error {
+	t := &task{
+		Processed: true,
+	}
+	err := p.db.Model(t).Where("task_id = ?", taskID).Where("project_id = ?", projectID).Updates(t).Error
+	return errors.Wrap(err, "failed to update task")
 }
 
 func (p *DB) DeleteTask(projectID uint64, taskID common.Hash) error {
@@ -120,9 +136,20 @@ func (p *DB) DeleteTask(projectID uint64, taskID common.Hash) error {
 	return errors.Wrap(err, "failed to delete task")
 }
 
-func New(db *gorm.DB) (*DB, error) {
+func (p *DB) UnprocessedTask() (uint64, common.Hash, error) {
+	t := task{}
+	if err := p.db.Order("created_at ASC").Where("processed = false").First(&t).Error; err != nil {
+		if err == gorm.ErrRecordNotFound {
+			return 0, common.Hash{}, nil
+		}
+		return 0, common.Hash{}, errors.Wrap(err, "failed to query unprocessed task")
+	}
+	return t.ProjectID, t.TaskID, nil
+}
+
+func New(db *gorm.DB, prover common.Address) (*DB, error) {
 	if err := db.AutoMigrate(&task{}, &scannedBlockNumber{}); err != nil {
 		return nil, errors.Wrap(err, "failed to migrate model")
 	}
-	return &DB{db}, nil
+	return &DB{db: db, prover: prover}, nil
 }
