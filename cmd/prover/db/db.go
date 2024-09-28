207bd3fff5eb4cea60ba7e2df97d4520c5734da6
huangzhiran <zhiran@iotex.me>
2024-09-28 18:09:37 +0800

    change project manager to sqlite & delete local model support

--- cmd/prover/db/db.go
+++ cmd/prover/db/db.go
@@ -1,4 +1,4 @@
-package postgres
+package db
 
 import (
 	"github.com/ethereum/go-ethereum/common"
@@ -10,6 +10,20 @@ import (
 type scannedBlockNumber struct {
 	gorm.Model
 	Number uint64 `gorm:"not null"`
+}
+
+type project struct {
+	gorm.Model
+	ProjectID uint64      `gorm:"uniqueIndex:project_id,not null"`
+	URI       string      `gorm:"not null"`
+	Hash      common.Hash `gorm:"not null"`
+}
+
+type projectFile struct {
+	gorm.Model
+	ProjectID uint64      `gorm:"uniqueIndex:project_id,not null"`
+	File      []byte      `gorm:"not null"`
+	Hash      common.Hash `gorm:"not null"`
 }
 
 type task struct {
@@ -40,31 +54,70 @@ func (p *DB) UpsertScannedBlockNumber(number uint64) error {
 		},
 		Number: number,
 	}
-	if err := p.db.Clauses(clause.OnConflict{
+	err := p.db.Clauses(clause.OnConflict{
 		Columns:   []clause.Column{{Name: "id"}},
 		DoUpdates: clause.AssignmentColumns([]string{"number"}),
-	}).Create(&t).Error; err != nil {
-		return errors.Wrap(err, "failed to upsert scanned block number")
-	}
-	return nil
+	}).Create(&t).Error
+	return errors.Wrap(err, "failed to upsert scanned block number")
 }
 
-func (p *DB) CreateTask(taskID common.Hash, projectID uint64) error {
+func (p *DB) Project(projectID uint64) (string, common.Hash, error) {
+	t := project{}
+	if err := p.db.Where("project_id = ?", projectID).First(&t).Error; err != nil {
+		return "", common.Hash{}, errors.Wrap(err, "failed to query project")
+	}
+	return t.URI, t.Hash, nil
+}
+
+func (p *DB) UpsertProject(projectID uint64, uri string, hash common.Hash) error {
+	t := project{
+		ProjectID: projectID,
+		URI:       uri,
+		Hash:      hash,
+	}
+	err := p.db.Clauses(clause.OnConflict{
+		Columns:   []clause.Column{{Name: "project_id"}},
+		DoUpdates: clause.AssignmentColumns([]string{"uri", "hash"}),
+	}).Create(&t).Error
+	return errors.Wrap(err, "failed to upsert project")
+}
+
+func (p *DB) ProjectFile(projectID uint64) ([]byte, common.Hash, error) {
+	t := projectFile{}
+	if err := p.db.Where("project_id = ?", projectID).First(&t).Error; err != nil {
+		if err == gorm.ErrRecordNotFound {
+			return nil, common.Hash{}, nil
+		}
+		return nil, common.Hash{}, errors.Wrap(err, "failed to query project file")
+	}
+	return t.File, t.Hash, nil
+}
+
+func (p *DB) UpsertProjectFile(projectID uint64, file []byte, hash common.Hash) error {
+	t := projectFile{
+		ProjectID: projectID,
+		File:      file,
+		Hash:      hash,
+	}
+	err := p.db.Clauses(clause.OnConflict{
+		Columns:   []clause.Column{{Name: "project_id"}},
+		DoUpdates: clause.AssignmentColumns([]string{"file", "hash"}),
+	}).Create(&t).Error
+	return errors.Wrap(err, "failed to upsert project file")
+}
+
+func (p *DB) CreateTask(projectID uint64, taskID common.Hash) error {
 	t := &task{
 		TaskID:    taskID,
 		ProjectID: projectID,
 	}
-	if err := p.db.Create(t).Error; err != nil {
-		return errors.Wrap(err, "failed to create task")
-	}
-	return nil
+	err := p.db.Create(t).Error
+	return errors.Wrap(err, "failed to create task")
 }
 
-func (p *DB) DeleteTask(taskID common.Hash, projectID uint64) error {
-	if err := p.db.Where("task_id = ?", taskID).Where("project_id = ?", projectID).Delete(&task{}).Error; err != nil {
-		return errors.Wrap(err, "failed to delete task")
-	}
-	return nil
+func (p *DB) DeleteTask(projectID uint64, taskID common.Hash) error {
+	err := p.db.Where("task_id = ?", taskID).Where("project_id = ?", projectID).Delete(&task{}).Error
+	return errors.Wrap(err, "failed to delete task")
 }
 
 func New(db *gorm.DB) (*DB, error) {
