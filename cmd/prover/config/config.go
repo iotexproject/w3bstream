68a13a0834c326a25d097c2887a839509e7c8dd7
huangzhiran <zhiran@iotex.me>
2024-09-29 17:22:17 +0800

    prover main logic

--- cmd/prover/config/config.go
+++ cmd/prover/config/config.go
@@ -8,16 +8,17 @@ import (
 )
 
 type Config struct {
-	LogLevel             slog.Level `env:"LOG_LEVEL,optional"`
-	VMEndpoints          string     `env:"VM_ENDPOINTS"`
-	DatabaseDSN          string     `env:"DATABASE_DSN"`
-	ChainEndpoint        string     `env:"CHAIN_ENDPOINT,optional"`
-	ProjectContractAddr  string     `env:"PROJECT_CONTRACT_ADDRESS,optional"`
-	ProverContractAddr   string     `env:"PROVER_CONTRACT_ADDRESS,optional"`
-	ProverOperatorPriKey string     `env:"PROVER_OPERATOR_PRIVATE_KEY,optional"`
-	BeginningBlockNumber uint64     `env:"BEGINNING_BLOCK_NUMBER,optional"`
-	LocalDBDir           string     `env:"LOCAL_DB_DIRECTORY,optional"`
-	env                  string     `env:"-"`
+	LogLevel                slog.Level `env:"LOG_LEVEL,optional"`
+	VMEndpoints             string     `env:"VM_ENDPOINTS"`
+	DatasourceDSN           string     `env:"DATASOURCE_DSN"`
+	ChainEndpoint           string     `env:"CHAIN_ENDPOINT,optional"`
+	ProjectContractAddr     string     `env:"PROJECT_CONTRACT_ADDRESS,optional"`
+	RouterContractAddr      string     `env:"ROUTER_CONTRACT_ADDRESS,optional"`
+	TaskManagerContractAddr string     `env:"TASK_MANAGER_CONTRACT_ADDRESS,optional"`
+	ProverOperatorPriKey    string     `env:"PROVER_OPERATOR_PRIVATE_KEY,optional"`
+	BeginningBlockNumber    uint64     `env:"BEGINNING_BLOCK_NUMBER,optional"`
+	LocalDBDir              string     `env:"LOCAL_DB_DIRECTORY,optional"`
+	env                     string     `env:"-"`
 }
 
 var (
@@ -25,9 +26,8 @@ var (
 		LogLevel:             slog.LevelInfo,
 		VMEndpoints:          `{"1":"risc0:4001","2":"halo2:4001","3":"zkwasm:4001","4":"wasm:4001"}`,
 		ChainEndpoint:        "https://babel-api.testnet.iotex.io",
-		DatabaseDSN:          "postgres://test_user:test_passwd@postgres:5432/test?sslmode=disable",
-		ProjectContractAddr:  "0xCBb7a80983Fd3405972F700101A82DB6304C6547",
-		ProverContractAddr:   "0x6B544a7603cead52AdfD99AA64B3d798083cc4CC",
+		DatasourceDSN:        "postgres://test_user:test_passwd@postgres:5432/test?sslmode=disable",
+		ProjectContractAddr:  "0x6B544a7603cead52AdfD99AA64B3d798083cc4CC",
 		ProverOperatorPriKey: "a5f4e99aa80342d5451e8f8fd0dc357ccddb70d3827428fb1fc366f70833497f",
 		BeginningBlockNumber: 20000000,
 		LocalDBDir:           "./local_db",
