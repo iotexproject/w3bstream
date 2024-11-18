f5247b9cf0e8c3a00f0a21b97c970b58f912a589
huangzhiran <zhiran@iotex.me>
2024-11-18 14:43:38 +0800

    del unuse code

--- task/task.go
+++ task/task.go
@@ -3,9 +3,7 @@ package task
 import (
 	"bytes"
 	"crypto/ecdsa"
-	"encoding/binary"
 	"encoding/json"
-	"time"
 
 	"github.com/ethereum/go-ethereum/common"
 	"github.com/ethereum/go-ethereum/crypto"
@@ -58,77 +56,4 @@ func (t *Task) Hash() (common.Hash, error) {
 	}
 
 	return crypto.Keccak256Hash(j), nil
-}
-
-type StateLog struct {
-	TaskID    uint64
-	ProjectID uint64
-	State     State
-	Comment   string
-	Result    []byte
-	Signature string
-	ProverID  uint64
-	CreatedAt time.Time
-}
-
-func (l *StateLog) SignerAddress(task *Task) (common.Address, error) {
-	buf := bytes.NewBuffer(nil)
-
-	if err := binary.Write(buf, binary.BigEndian, task.ID); err != nil {
-		return common.Address{}, errors.Wrap(err, "failed to write binary")
-	}
-	if err := binary.Write(buf, binary.BigEndian, task.ProjectID); err != nil {
-		return common.Address{}, errors.Wrap(err, "failed to write binary")
-	}
-	if _, err := buf.Write(task.DeviceID[:]); err != nil {
-		return common.Address{}, errors.Wrap(err, "failed to write bytes buffer")
-	}
-	if _, err := buf.Write(crypto.Keccak256Hash(task.Payloads...).Bytes()); err != nil {
-		return common.Address{}, errors.Wrap(err, "failed to write bytes buffer")
-	}
-	if _, err := buf.Write(l.Result); err != nil {
-		return common.Address{}, errors.Wrap(err, "failed to write bytes buffer")
-	}
-
-	h := crypto.Keccak256Hash(buf.Bytes())
-	sigpk, err := crypto.Ecrecover(h.Bytes(), task.Signature)
-	if err != nil {
-		return common.Address{}, errors.Wrap(err, "failed to recover public key")
-	}
-
-	publicKey, err := crypto.UnmarshalPubkey(sigpk)
-	if err != nil {
-		return common.Address{}, errors.Wrap(err, "failed to unmarshal public key")
-	}
-	return crypto.PubkeyToAddress(*publicKey), nil
-}
-
-type State uint8
-
-const (
-	StateInvalid State = iota
-	StatePacked
-	StateDispatched
-	_
-	StateProved
-	_
-	StateOutputted
-	StateFailed
-)
-
-func (s State) String() string {
-	switch s {
-	case StatePacked:
-		return "packed"
-	case StateDispatched:
-		return "dispatched"
-	case StateProved:
-		return "proved"
-	case StateOutputted:
-		return "outputted"
-	case StateFailed:
-		return "failed"
-	default:
-		return "invalid"
-	}
 }
