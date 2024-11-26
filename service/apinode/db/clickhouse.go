caee7238894a03c5a9accb01bfa7f240e1f9e794
huangzhiran <zhiran@iotex.me>
2024-11-26 14:12:17 +0800

    support multi alg

--- service/apinode/db/clickhouse.go
+++ service/apinode/db/clickhouse.go
@@ -54,8 +54,8 @@ func migrateCH(conn driver.Conn) error {
             project_version String NOT NULL,
             payloads Array(UInt8) NOT NULL,
             signature Array(UInt8) NOT NULL,
-        	hash_algorithm String NOT NULL,
-        	create_at DateTime NOT NULL
+            algorithm String NOT NULL,
+            create_at DateTime NOT NULL
         )
         ENGINE = ReplacingMergeTree()
         PRIMARY KEY task_id
