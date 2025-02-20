6a0d235acaeb7a331b0f573b10e8723bfd9849e4
huangzhiran <zhiran@iotex.me>
2025-02-20 16:34:52 +0800

    tmp

--- smartcontracts/contracts/MovementBatchVerifier.sol
+++ smartcontracts/contracts/MovementBatchVerifier.sol
@@ -8,7 +8,7 @@ pragma solidity ^0.8.0;
 /// (256 bytes) and compressed (128 bytes) format. A view function is provided
 /// to compress proofs.
 /// @notice See <https://2π.com/23/bn254-compression> for further explanation.
-contract Verifier {
+contract MovementBatchVerifier {
     /// Some of the provided public input values are larger than the field modulus.
     /// @dev Public input elements are not automatically reduced, as this is can be
     /// a dangerous source of bugs.
